109420b2de7f3714a5e9ccaa540019f696f2c109
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-27 14:46:56 +0100

    add explanations to some keymaps

--- Config/nvim/lua/yushi_61/remap.lua
+++ Config/nvim/lua/yushi_61/remap.lua
@@ -9,16 +9,11 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- move the lines side by side
 vim.keymap.set("n", "J", "mzJ`z")
 
--- idk what this does so I'll cancel them for now
--- vim.keymap.set("n", "<C-d>", "<C-d>zz")
--- vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- vim.keymap.set("n", "n", "nzzzv")
--- vim.keymap.set("n", "N", "Nzzzv")
 
--- idk what this does, for me now it's the same as p
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- copy the highlighted text in the clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- delete the highlighted text
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- cancel the Q command I guess
@@ -30,15 +25,16 @@ vim.keymap.set("n", "<leader>f", function()
 end)
 
 
--- commands that I don't know what it does
+-- fz find in the whole file 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- fz find in the same line
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 
--- change the line or the letter faster
+-- fz find to change in the whole file easily
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- make a file executable 
+-- make a file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
