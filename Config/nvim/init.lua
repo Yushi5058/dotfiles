2fbecd3b2e6cd8cf486d33d341fb24fba5ac1cd9
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-28 16:27:06 +0100

    update netrw and add augroups and autoremove whitespace

--- Config/nvim/init.lua
+++ Config/nvim/init.lua
@@ -3,6 +3,41 @@ require("yushi_61.set")
 
 
 
+local augroup = vim.api.nvim_create_augroup
+local YushiGroup = augroup('Yushi', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = YushiGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+
+
+
+
 
 
 
