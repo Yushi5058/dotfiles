1bf3c8b889e5df608fd221311d143f7541a6d33d
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-28 16:33:24 +0100

    add zen-mode plugin

--- Config/nvim/lua/yushi_61/packer.lua
+++ Config/nvim/lua/yushi_61/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -14,79 +14,80 @@ local packer_bootstrap = ensure_packer()
 
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-use({
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  })
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
 
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use "terrortylor/nvim-comment"
-  require('nvim_comment').setup()
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use "terrortylor/nvim-comment"
+    require('nvim_comment').setup()
 
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use("theprimeagen/refactoring.nvim")
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-  'lewis6991/gitsigns.nvim',
-  config = function()
-      require('gitsigns').setup {
-           signs = {
-    add          = { text = '+' },
-    delete       = { text = '-' },
-    untracked    = { text = '?' },
-  },
-      }
-  end
-}
-  use("nvim-treesitter/nvim-treesitter-context")
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v1.x',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use("theprimeagen/refactoring.nvim")
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup {
+                signs = {
+                    add       = { text = '+' },
+                    delete    = { text = '-' },
+                    untracked = { text = '?' },
+                },
+            }
+        end
+    }
+    use("nvim-treesitter/nvim-treesitter-context")
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
-  use("eandrju/cellular-automaton.nvim")
-  use("jiangmiao/auto-pairs")
-  use ("alvan/vim-closetag")
-  use ("wakatime/vim-wakatime")
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+    use("eandrju/cellular-automaton.nvim")
+    use("jiangmiao/auto-pairs")
+    use("alvan/vim-closetag")
+    use("folke/zen-mode.nvim")
+    use("wakatime/vim-wakatime")
 
 
- -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
