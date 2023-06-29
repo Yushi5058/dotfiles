453db72fd83dd0ca747744fa19d2fa7029baf6ce
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-06-29 16:17:50 +0100

    update init file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+require('lua.config') -- Configuration generale
+require('lua.plugins') -- Gestion des plugins
+require('lua.mappings') -- Raccourcis clavier
+require('lua.colors') -- Themes et couleurs
+require('lua.lsp') -- Configuration LSP
+require('lua.utils') -- Fonctions utilitaires
