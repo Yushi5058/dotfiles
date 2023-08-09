e599491966c0ba38bc9ba6c6b5af96cb882a9ee7
Yushi <yushi_61@proton.me>
2023-08-09 17:19:34 +0100

    update nvim files

--- .config/nvim/after/plugin/fugitive.lua
+++
@@ -1,9 +0,0 @@
-m_set_keymap('n', '<leader>gs', ':G status', {noremap = true, silent = true})
--- Raccourci pour push origin main
--- vim.api.nvim_set_keymap('n', '<leader>gp', ':G push origin main<CR>', { noremap = true })
---
--- -- Raccourci pour commit avec un message
--- vim.api.nvim_set_keymap('n', '<leader>gc', ':G commit -m', { noremap = true })
---
--- -- Raccourci pour ajouter un fichier
--- vim.api.nvim_set_keymap('n', '<leader>ga', ':G add', { noremap = true })
---
+++ .config/nvim/plugin_config/treesitter.lua
@@ -0,0 +1,15 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "lua", "html", "javascript", "markdown", "scss", "typescript", "vue", "vim" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+  auto_install = true,
+  indent = {
+  enable = true
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
