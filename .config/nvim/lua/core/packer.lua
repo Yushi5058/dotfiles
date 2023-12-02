a3ce315257c49fd7a5fad1d9af182a35bd820317
Yushi <yushi_61@proton.me>
2023-12-02 22:53:00 +0100

    fix telescope issue

--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -5,7 +5,7 @@ return require('packer').startup(function(use)
 
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
     use { "catppuccin/nvim", as = "catppuccin" }
