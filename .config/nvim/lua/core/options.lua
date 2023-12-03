7b4625b754d41b68b7632a9e36e788c6862538b9
Yushi <yushi_61@proton.me>
2023-12-03 20:49:26 +0100

    update options for better workflow

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,32 +1,46 @@
-vim.opt.guicursor = ""
+vim.g.mapleader = " "
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.title = true
+
+vim.opt.number = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.wrap = false
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
-vim.opt.wrap = false
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+
+vim.opt.shell="fish"
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.opt.hlsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
 
-vim.opt.updatetime = 50
+vim.opt.scrolloff = 10
+vim.opt.laststatus = 2
 
-vim.opt.colorcolumn = "80"
+-- disable mouse
+vim.opt.mouse = ""
 
