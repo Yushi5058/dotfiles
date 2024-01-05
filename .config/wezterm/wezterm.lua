0610664e39608d4f4736614edd515266e0792653
Yushi <yushi_61@proton.me>
2024-01-05 19:23:17 +0100

    feat: change wezterm font to Hack NF

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = {
-	font = wezterm.font("JetBrains Mono"),
+	font = wezterm.font("Hack"),
 	font_size = 14.0,
 	color_scheme = "Catppuccin Frappe",
 }
