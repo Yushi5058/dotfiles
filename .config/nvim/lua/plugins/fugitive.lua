06e4a2fa627a94e539f62eed190843668c3c6c5d
Yushi <yushi_61@proton.me>
2024-01-05 22:20:16 +0100

    fix: update fugitive keymaps

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,9 +3,9 @@ return {
 	config = function()
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ga", "<cmd>G add")
-		keymap.set("n", "<leader>gc", "<cmd>G commit -m")
-		keymap.set("n", "<leader>gpush", "<cmd>G push origin main")
+		keymap.set("n", "<leader>ga", ":G add ")
+		keymap.set("n", "<leader>gc", "<:G commit -m ")
+		keymap.set("n", "<leader>gpush", "<cmd>G push origin main<cr>")
 		keymap.set("n", "<leader>gs", "<cmd>G status<cr>")
 	end,
 }
