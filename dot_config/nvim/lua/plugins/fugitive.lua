799584b58d82a4af8055095a2311c1bdaca9b6dc
Yushi <yushi_61@proton.me>
2024-01-10 13:27:07 +0100

    feat:add neovim config

---
+++ dot_config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,11 @@
+return {
+  "tpope/vim-fugitive",
+  config = function()
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>ga", ":G add ")
+    keymap.set("n", "<leader>gc", ":G commit -m ")
+    keymap.set("n", "<leader>gp", "<cmd>push origin main<cr>")
+    keymap.set("n", "<leader>gs", "<cmd>G status<cr>")
+  end,
+}
