b165670d8403998ad24aec56a74f96dcb5208c22
Yushi <yushi_61@proton.me>
2024-01-10 13:17:48 +0100

    feat:delete all

--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "echasnovski/mini.pairs", enabled = false },
-	{ "rafamadriz/friendly-snippets", enabled = false },
-	{ "akinsho/bufferline.nvim", enabled = false },
-}
