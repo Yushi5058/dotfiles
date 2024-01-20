37d8adcbaa8510a0ee3cd3b02c6295fefbc5bd76
Yushi <yushi_61@proton.me>
2024-01-20 11:30:40 +0100

    fix: update wilde configuration nvim

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -47,5 +47,15 @@ return {
   config = function()
     local wilder = require("wilder")
     wilder.setup({ modes = { ":", "/", "?" } })
+    wilder.set_option("pipeline", {
+      wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
+    })
+
+    wilder.set_option(
+      "renderer",
+      wilder.wildmenu_renderer({
+        highlighter = wilder.basic_highlighter(),
+      })
+    )
   end,
 }
