3076e1b84d102ba8b87722caa22e9944b18a4745
Yushi <yushi_61@proton.me>
2024-01-24 10:37:44 +0100

    feat: add mini animate and customize wilder

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -7,6 +7,7 @@ return {
 			require("mini.indentscope").setup()
 			require("mini.pairs").setup()
 			require("mini.surround").setup()
+			require("mini.animate").setup()
 		end,
 	},
 	{
@@ -85,9 +86,47 @@ return {
 	},
 	{
 		"gelguy/wilder.nvim",
+		dependencies = { "catppuccin/nvim" },
 		config = function()
 			local wilder = require("wilder")
+			local frappe = require("catppuccin.palettes").get_palette("frappe")
+
+			-- Create a highlight group for the popup menu
+			local text_highlight = wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = frappe.text } })
+			local mauve_highlight =
+				wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = frappe.mauve } })
 			wilder.setup({ modes = { ":", "/", "?" } })
+
+			-- Enable fuzzy matching for commands and buffers
+			wilder.set_option("pipeline", {
+				wilder.branch(
+					wilder.cmdline_pipeline({
+						fuzzy = 1,
+					}),
+					wilder.vim_search_pipeline({
+						fuzzy = 1,
+					})
+				),
+			})
+
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+					highlighter = wilder.basic_highlighter(),
+					highlights = {
+						default = text_highlight,
+						border = mauve_highlight,
+						accent = mauve_highlight,
+					},
+					pumblend = 5,
+					min_width = "100%",
+					min_height = "25%",
+					max_height = "25%",
+					border = "rounded",
+					left = { " ", wilder.popupmenu_devicons() },
+					right = { " ", wilder.popupmenu_scrollbar() },
+				}))
+			)
 		end,
 	},
 }
