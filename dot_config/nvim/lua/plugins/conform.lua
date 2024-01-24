c9b304b294d9951d4f7a26b67645f2e59fe5bee2
Yushi <yushi_61@proton.me>
2024-01-24 11:32:25 +0100

    fix: remove prettierd

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,30 +1,27 @@
-return
-{
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function() 
-      require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-   javascript = { { "prettierd", "prettier" } },
-  },
-})
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettier" } },
+			},
+		})
 
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-
-
-    require("conform").setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-})
-  end,
-})
-end
+				require("conform").setup({
+					format_on_save = {
+						-- These options will be passed to conform.format()
+						timeout_ms = 500,
+						lsp_fallback = true,
+					},
+				})
+			end,
+		})
+	end,
 }
