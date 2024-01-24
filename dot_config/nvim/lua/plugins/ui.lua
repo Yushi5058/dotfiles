a9f6c0fd2218497311b619d61b165542ea1b0a64
Yushi <yushi_61@proton.me>
2024-01-24 11:38:46 +0100

    fix: remove unecessary wilder plugin

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -84,49 +84,4 @@ return {
 			end,
 		},
 	},
-	{
-		"gelguy/wilder.nvim",
-		dependencies = { "catppuccin/nvim" },
-		config = function()
-			local wilder = require("wilder")
-			local frappe = require("catppuccin.palettes").get_palette("frappe")
-
-			-- Create a highlight group for the popup menu
-			local text_highlight = wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = frappe.text } })
-			local mauve_highlight =
-				wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = frappe.mauve } })
-			wilder.setup({ modes = { ":", "/", "?" } })
-
-			-- Enable fuzzy matching for commands and buffers
-			wilder.set_option("pipeline", {
-				wilder.branch(
-					wilder.cmdline_pipeline({
-						fuzzy = 1,
-					}),
-					wilder.vim_search_pipeline({
-						fuzzy = 1,
-					})
-				),
-			})
-
-			wilder.set_option(
-				"renderer",
-				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-					highlighter = wilder.basic_highlighter(),
-					highlights = {
-						default = text_highlight,
-						border = mauve_highlight,
-						accent = mauve_highlight,
-					},
-					pumblend = 5,
-					min_width = "100%",
-					min_height = "25%",
-					max_height = "25%",
-					border = "rounded",
-					left = { " ", wilder.popupmenu_devicons() },
-					right = { " ", wilder.popupmenu_scrollbar() },
-				}))
-			)
-		end,
-	},
 }
