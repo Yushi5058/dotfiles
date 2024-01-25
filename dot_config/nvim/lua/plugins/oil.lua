efb35a2ed789b8571789bf288cb69445781c2f2c
Yushi <yushi_61@proton.me>
2024-01-25 16:34:36 +0100

    fix: configure oil

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -4,25 +4,42 @@ return {
 	opts = {},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keymaps = {
-		["g?"] = "actions.show_help",
-		["<CR>"] = "actions.select",
-		["<C-s>"] = "actions.select_vsplit",
-		["<C-h>"] = "actions.select_split",
-		["<C-t>"] = "actions.select_tab",
-		["<C-p>"] = "actions.preview",
-		["<C-c>"] = "actions.close",
-		["<C-l>"] = "actions.refresh",
-		["-"] = "actions.parent",
-		["_"] = "actions.open_cwd",
-		["`"] = "actions.cd",
-		["~"] = "actions.tcd",
-		["gs"] = "actions.change_sort",
-		["gx"] = "actions.open_external",
-		["g."] = "actions.toggle_hidden",
-		["g\\"] = "actions.toggle_trash",
-	},
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = "actions.select_vsplit",
+				["<C-h>"] = "actions.select_split",
+				["<C-t>"] = "actions.select_tab",
+				["<C-p>"] = "actions.preview",
+				["<Esc>"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["<leader>pv"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["gs"] = "actions.change_sort",
+				["gx"] = "actions.open_external",
+				["g."] = "actions.toggle_hidden",
+				["g\\"] = "actions.toggle_trash",
+			},
+			use_default_keymaps = false,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+			float = {
+				-- Padding around the floating window
+				padding = 2,
+				max_width = 0,
+				max_height = 0,
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+			},
+		})
 	end,
 }
