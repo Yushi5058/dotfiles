8bf146c806866adf72427bede9011b40a9201cfa
Yushi <yushi_61@proton.me>
2024-01-26 18:17:58 +0100

    feat: update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+local act = wezterm.action
 local config = {}
 -- Use config builder object if possible
 if wezterm.config_builder then
@@ -14,12 +16,20 @@ config.font = wezterm.font_with_fallback({
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
+config.use_dead_keys = false
 config.default_workspace = "main"
+
+config.window_frame = {
+	font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
+}
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
 	saturation = 0.24,
 	brightness = 0.5,
 }
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 return config
