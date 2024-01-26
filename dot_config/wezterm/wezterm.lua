6a111d885066c215ffc405b0213b96bfff2145ae
Yushi <yushi_61@proton.me>
2024-01-26 12:30:25 +0100

    feat: update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,24 @@
 local wezterm = require("wezterm")
-local config = {
-	font = wezterm.font("Hack Nerd Font"),
-	font_size = 14.0,
-	color_scheme = "Catppuccin Frappe",
+local config = {}
+-- Use config builder object if possible
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- settings
+config.font = wezterm.font_with_fallback({
+	{ family = "Hack Nerd Font", scale = 1.24, weight = "Medium" },
+	{ family = "CaskaydiaCove Nerd Font", scale = 1.2 },
+})
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+config.default_workspace = "main"
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+	saturation = 0.24,
+	brightness = 0.5,
 }
 
 return config
