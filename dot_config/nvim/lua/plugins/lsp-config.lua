15139a6c5ef2ac5c0b7998fb3316c9c95a0f59a6
Yushi <yushi_61@proton.me>
2024-01-29 20:54:29 +0100

    feat: update lsp configuration

---
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "tsserver", "remark_ls", "ruby_ls" },
+				auto_install = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.remark_ls.setup({})
+			lspconfig.ruby_ls.setup({})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
