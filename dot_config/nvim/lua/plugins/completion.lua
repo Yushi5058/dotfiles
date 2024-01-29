33bcbf1104191235b1599e844e917e0a679194b0
Yushi <yushi_61@proton.me>
2024-01-29 21:39:21 +0100

    feat: update cmp configuration

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -1,47 +1,82 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+	"hrsh7th/cmp-nvim-lsp",
+	event = { "BufReadPost", "BufNewFile" },
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip", max_item_count = 3 }, -- snippets
-					{ name = "buffer", max_item_count = 5 }, -- text within current buffer
-					{ name = "path", max_item_count = 3 }, -- file system paths
-				}),
-			})
-		end,
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+		"saadparwaiz1/cmp_luasnip",
+		"onsails/lspkind.nvim",
 	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
+
+		-- load snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
+				["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
+				["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+				["<C-c>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
+			}),
+
+			-- sources for autocompletion
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- lsp
+				{ name = "luasnip", max_item_count = 3 }, -- snippets
+				{ name = "buffer", max_item_count = 5 }, -- text within current buffer
+				{ name = "path", max_item_count = 3 }, -- file system paths
+			}),
+
+			-- Enable pictogram icons for lsp/autocompletion
+			formatting = {
+				expandable_indicator = true,
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+			experimental = {
+				ghost_text = false,
+			},
+		})
+	end,
 }
