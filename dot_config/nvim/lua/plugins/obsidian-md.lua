3354eaa58690bf36ceb903b2f1a361581bd7f960
Yushi <yushi_61@proton.me>
2024-01-29 19:32:20 +0100

    feat: update obsidian plugin

--- dot_config/nvim/lua/plugins/obsidian-md.lua
+++ dot_config/nvim/lua/plugins/obsidian-md.lua
@@ -1,44 +1,25 @@
 return {
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		keys = {
-			{ "<c-o>", "<cmd>ObsidianOpen<cr>" },
-			{ "<c-n>", "<cmd>ObsidianNew<cr>" },
-			{ "<leader>fo", "<cmd>ObsidianSearch<cr>" },
-			{ "<leader>fs", "<cmd>ObsidianQuickSwitch<cr>" },
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-			"nvim-telescope/telescope.nvim",
-			"nvim-treesitter",
-		},
-		opts = {
-			dir = "~/Obsidian Vault/",
-			completion = {
-				nvim_cmp = true,
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-telescope/telescope.nvim",
+		"preservim/vim-markdown",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "personal",
+				path = "~/Obsidian Vault/",
 			},
 		},
-		config = function(_, opts)
-			require("obsidian").setup(opts)
-		end,
-		follow_url_func = function(url)
-			-- Open the URL in the default web browser.
-			--	vim.fn.jobstart({ "open", url }) -- Mac OS
-			vim.fn.jobstart({ "xdg-open", url }) -- linux
-		end,
-
-		finder = "telescope.nvim",
 	},
-
-	{
-		"ellisonleao/glow.nvim",
-		config = function()
-			require("glow").setup()
-			vim.keymap.set("n", "<leader>md", "<cmd>Glow<cr>")
-		end,
-	},
+	config = function()
+		require("obsidian").setup()
+		vim.keymap.set("n", "<c-n>", "<cmd>ObsidianNew<cr>")
+		vim.keymap.set("n", "<c-o", "<cmd>ObsidianQuickSwitch<cr>")
+	end,
 }
