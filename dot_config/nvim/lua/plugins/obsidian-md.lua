b2ef35fb5e72d480105eb03278bee82146a5735c
Yushi <yushi_61@proton.me>
2024-01-29 19:36:46 +0100

    feat: add glow plugin

--- dot_config/nvim/lua/plugins/obsidian-md.lua
+++ dot_config/nvim/lua/plugins/obsidian-md.lua
@@ -1,25 +1,34 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-		"nvim-telescope/telescope.nvim",
-		"preservim/vim-markdown",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/Obsidian Vault/",
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+			"nvim-telescope/telescope.nvim",
+			"preservim/vim-markdown",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/Obsidian Vault/",
+				},
 			},
 		},
+		config = function()
+			require("obsidian").setup()
+			vim.keymap.set("n", "<c-n>", "<cmd>ObsidianNew<cr>")
+			vim.keymap.set("n", "<c-o", "<cmd>ObsidianQuickSwitch<cr>")
+		end,
 	},
-	config = function()
-		require("obsidian").setup()
-		vim.keymap.set("n", "<c-n>", "<cmd>ObsidianNew<cr>")
-		vim.keymap.set("n", "<c-o", "<cmd>ObsidianQuickSwitch<cr>")
-	end,
+	{
+		"ellisonleao/glow.nvim",
+		config = function()
+			require("glow").setup()
+			vim.keymap.set("n", "<c-g", "<cmd>Glow<cr>")
+		end,
+	},
 }
