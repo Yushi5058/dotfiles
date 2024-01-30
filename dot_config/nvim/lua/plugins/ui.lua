2a3bc82e8d8b4a41ace6bea0bf2d73ae071bec0e
Yushi <yushi_61@proton.me>
2024-01-30 23:48:34 +0100

    feat: add gitsigns keymaps

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -22,6 +22,8 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>")
+			vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<cr>")
 		end,
 	},
 	{
