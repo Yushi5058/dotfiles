242c3640c729d304cd789973ae317dff0a4aa6df
Yushi <yushi_61@proton.me>
2024-01-31 12:53:34 +0100

    feat: update autocmds

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,36 +1,21 @@
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local function augroup(name)
+	return vim.api.nvim_create_augroup("yushi" .. name, { clear = true })
+end
+
+-- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
-	group = highlight_group,
-	pattern = "*",
-})
--- Enable spell check and text wrap for certain filetypes
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = vim.api.nvim_create_augroup("edit_text", { clear = true }),
-	pattern = { "gitcommit", "markdown", "txt" },
-	callback = function()
-		vim.wo.wrap = true
-		vim.wo.spell = true
-	end,
 })
 
--- Disable the concealing in some file formats
--- The default conceallevel is 3 in LazyVim
+-- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "*" },
+	group = augroup("wrap_spell"),
+	pattern = { "gitcommit", "markdown" },
 	callback = function()
-		vim.wo.conceallevel = 2
-	end,
-})
-
--- Remove colorcolumn when oil is enabled
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "oil",
-	callback = function()
-		vim.opt_local.colorcolumn = ""
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
 	end,
 })
