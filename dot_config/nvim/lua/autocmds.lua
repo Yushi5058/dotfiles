93fd156109dae6f4cc59b3e400ecf5692ff2c233
Yushi <yushi_61@proton.me>
2024-01-31 13:22:31 +0100

    feat: update conceallevel

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -13,9 +13,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup("wrap_spell"),
-	pattern = { "gitcommit", "markdown" },
+	pattern = { "markdown" },
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.spell = true
+		vim.opt_conceallevel = 2
+	end,
+})
+
+-- default conceal level
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "*" },
+	callback = function()
+		vim.opt_conceallevel = 3
 	end,
 })
