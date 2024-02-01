062eba0b0ce72151760efd3055d668ee5dfb7e1c
Yushi <yushi_61@proton.me>
2024-02-01 19:57:37 +0100

    feat: add and configure neotest

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,21 @@
+return {
+
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"olimorris/neotest-rspec",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-rspec"),
+			},
+		})
+
+		vim.keymap.set("n", "<leader>nt", function()
+			require("neotest").run.run()
+		end)
+	end,
+}
