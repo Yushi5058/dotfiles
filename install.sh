d524ada80efbcec9a5f349a22df286b4db26aad4
Yushi <yushi_61@proton.me>
2024-02-02 12:02:47 +0100

    feat: add github username

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ set -e # -e: exit on error
 
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
+  GITHUB_USERNAME="Yushi5058"
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -- init -- apply "$GITHUB_USERNAME" -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -- init -- apply "$GITHUB_USERNAME" -b "$bin_dir"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
