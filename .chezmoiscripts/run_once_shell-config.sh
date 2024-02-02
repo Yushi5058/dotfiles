201e0e3d1fcb96ee253d4e27bedc321c28b07582
Yushi <yushi_61@proton.me>
2024-02-02 12:41:15 +0100

    feat: add zap install script

--- .chezmoiscripts/run_once_shell-config.sh
+++ .chezmoiscripts/run_once_shell-config.sh
@@ -11,6 +11,10 @@ chsh -s $(which zsh)
 
 echo "Default shell changed to zsh."
 
+# install zap zsh plugin manager
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
+
 # Run zap update to update and install plugins
 echo "Running 'zap update' to update and install Zsh plugins..."
 zap update all
