ace0f7150149e5e144ee447fd208e70219972bb6
Yushi <yushi_61@proton.me>
2024-02-04 18:00:58 +0100

    feat: update xplr config for icons

--- dot_config/xplr/init.lua
+++ dot_config/xplr/init.lua
@@ -3138,3 +3138,10 @@ return {
 -- >
 -- > If you still want to copy the entire configuration, make sure to put your
 -- > customization before the return statement.
+--
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
