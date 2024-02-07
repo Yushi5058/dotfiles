20ded8ca64140d3fec0b826e0a3511c4c1e94dfd
Yushi <yushi_61@proton.me>
2024-02-07 22:47:57 +0100

    feat: update lsp-cmp config and add yanky

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -16,11 +16,6 @@ keymap.set("n", "<leader>z", "<cmd>wq<cr>")
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Better paste / yank
-keymap.set("x", "<leader>p", [["_dP]])
-keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-keymap.set("n", "<leader>Y", [["+Y]])
-
 -- Increment/decrement
 keymap.set("n", "+", "<C-up>")
 keymap.set("n", "-", "<C-down")
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -92,4 +92,34 @@ return {
 			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
 		},
 	},
+	{
+		"gbprod/yanky.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		config = function()
+			require("yanky").setup({
+				ring = {
+					history_length = 100,
+					storage = "shada",
+					sync_with_numbered_registers = true,
+					cancel_event = "update",
+					ignore_registers = { "_" },
+					update_register_on_cycle = false,
+				},
+				system_clipboard = {
+					sync_with_ring = true,
+				},
+			})
+			vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+			vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+			vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+			vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+
+			vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+			vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+		end,
+	},
 }
