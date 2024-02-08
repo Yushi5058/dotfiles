e4bc3d6acf25c8c2c1e9571e98004daa8354d860
Yushi <yushi_61@proton.me>
2024-02-08 11:32:48 +0100

    feat: update nvim options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,29 +1,40 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-vim.o.breakindent = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
 
-vim.o.clipboard = "unnamedplus"
+vim.opt.clipboard = "unamed,unnamedplus"
+vim.opt.cursorline = true
 
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
+
+-- Better splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Disable mouse
+vim.opt.mouse = ""
+
+-- Enable ignorecase + smartcase for better searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 250
 vim.o.timeoutlen = 300
@@ -31,3 +42,12 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = "menuone,noselect"
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.guicursor = {
+	"n-v-c:block", -- Normal, visual, command-line: block cursor
+	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
+	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
+	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
+	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+}
