36aa8730bab22b80af1d7369b57b6bbd1d2c1300
Yushi <yushi_61@proton.me>
2024-02-10 01:38:36 +0100

    feat: fix sway-wm config keybinds

--- dot_config/sway/scripts/executable_night-control.sh
+++ dot_config/sway/scripts/executable_night-control.sh
@@ -2,10 +2,7 @@
 
 # before running kill any instances
 exec pkillall wlsunset
-# Run wlsunset with Casablanca / Morocco lat & long
-# exec wlsunset -l 33.5 -L -7.6
+# Run wlsunset
 exec wlsunset -t 3500
 
-# additionnal : execute caffeine-ng (workaround until I find a solution with sway)
-exec caffeine
 
