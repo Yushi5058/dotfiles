aa4f0b11eeb56f5de347ed132de262b745568007
Yushi <yushi_61@proton.me>
2024-02-15 13:02:06 +0100

    feat: migrate to wezterm

---
+++ dot_config/wezterm/config.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = {}
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Frappe"
+config.font = wezterm.font("MapleMono NF Regular")
+config.font_size = 16.0
+
+-- and finally, return the configuration to wezterm
+return config
