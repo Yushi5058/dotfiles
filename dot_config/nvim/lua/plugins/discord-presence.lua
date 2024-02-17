44cfe9751e0f8ad5837ff88bc214259f22353802
Yushi <yushi_61@proton.me>
2024-02-17 21:39:16 +0100

    feat: disable presence until the issue is resolved

--- dot_config/nvim/lua/plugins/discord-presence.lua
+++ dot_config/nvim/lua/plugins/discord-presence.lua
@@ -1,4 +1,8 @@
-return {
-	"andweeb/presence.nvim",
-	lazy = false,
-}
+-- reactive presence when the issue is being resolved
+-- https://github.com/andweeb/presence.nvim/issues/127
+--
+--
+-- return {
+-- 	"andweeb/presence.nvim",
+-- 	lazy = false,
+-- }
