0f44d9ffad0a2112cd59cf068b0bdb3618c15473
Yushi <yushi_61@proton.me>
2024-02-18 15:12:49 +0100

    feat: add legendary plugin

--- dot_config/nvim/lua/plugins/legendary.lua
+++ dot_config/nvim/lua/plugins/legendary.lua
@@ -2,4 +2,10 @@ return {
 	"mrjones2014/legendary.nvim",
 	priority = 10000,
 	lazy = false,
+	config = function()
+		require("legendary").setup({
+			keymaps = {},
+			lazy_nvim = { auto_register = true },
+		})
+	end,
 }
