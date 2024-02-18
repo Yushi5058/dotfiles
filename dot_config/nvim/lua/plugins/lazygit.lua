6638b860de09bcd27d2031806b010608cbdb284c
Yushi <yushi_61@proton.me>
2024-02-18 15:12:24 +0100

    fix: migrate keymaps to keys lazy function

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -4,8 +4,8 @@ return {
 	-- optional for floating window border decoration
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
-		end,
+	},
+	keys = {
+		{ "n", "<leader>gg", "<cmd>LazyGit<cr>" },
 	},
 }
