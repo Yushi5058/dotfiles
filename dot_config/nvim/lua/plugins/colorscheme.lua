2b816d3a54a0f1245833b6939ffb6a8d796b1389
Yushi <yushi_61@proton.me>
2024-02-22 20:57:27 +0100

    feat: update nvim config

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,23 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "frappe",
-		})
-		vim.cmd.colorscheme("catppuccin")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "frappe",
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+
+	-- Lua
+	{
+		"folke/twilight.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
