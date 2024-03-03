5f294cfdbec53d2a889053fbb0e60210772ce653
Yushi <yushi_61@proton.me>
2024-03-03 14:11:34 +0100

    feat: update keymaps

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -7,10 +7,6 @@ keymap.set({ "i", "v" }, "jk", "<Esc>")
 -- disable space in normal mode for leader key
 keymap.set({ "n", "v" }, "<space>", "<nop>")
 
--- save, quit, save & quit files with leader key
-keymap.set("n", "<leader>w", "<cmd>w<cr>")
-keymap.set("n", "<leader>q", "<cmd>q<cr>")
-
 -- Move line up/down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -22,11 +18,8 @@ keymap.set("n", "<leader>m", "<cmd>Mason<cr>")
 keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Diagnostic keymaps
-keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+-- Diagnostics :
+-- use telescope for diagnostics
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 keymap.set("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -43,21 +36,12 @@ keymap.set("n", "jk", "<cmd>nohlsearch<cr>")
 keymap.set("n", "ss", "<cmd>vsplit<cr>")
 -- keymap.set("n", "sh", "<cmd>split<cr>") for horizontal split
 
--- navigate between splits
-keymap.set("n", "sh", ":wincmd h<cr>")
-keymap.set("n", "sl", ":wincmd l<cr>")
-
 -- NOTE: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- Navigation splits
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
