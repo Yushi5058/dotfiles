0e3d69e7fd5ca88dd7db7e859ecfb5a84159a8b6
Yushi <yushi_61@proton.me>
2024-03-17 19:53:02 +0000

    feat: remove toggleterm

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -149,19 +149,4 @@ return {
 		"wakatime/vim-wakatime",
 		lazy = false,
 	},
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup()
-
-			local opts = { buffer = 0 }
-			vim.keymap.set("n", "<leader>t", ":ToggleTerm<cr>")
-			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-			vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-			vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-		end,
-	},
 }
