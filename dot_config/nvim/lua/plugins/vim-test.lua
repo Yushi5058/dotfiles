c01fd8994d4323fe67cd67771456c4eddd69c00a
Yushi <yushi_61@proton.me>
2024-04-12 12:15:25 +0000

    feat: update vimtest

--- dot_config/nvim/lua/plugins/vim-test.lua
+++ dot_config/nvim/lua/plugins/vim-test.lua
@@ -3,5 +3,6 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>tf", ":TestFile<cr>")
 		vim.keymap.set("n", "<leader>tn", ":TestNearest<cr>")
+		vim.cmd("let test#strategy = 'wezterm'")
 	end,
 }
