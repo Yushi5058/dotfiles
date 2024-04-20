37af2c770141b2dd1b7a6f7ce5774410f7b4fd52
Yushi <yushi_61@proton.me>
2024-04-20 13:21:16 +0100

    feat: migrate to neotest

--- dot_config/nvim/lua/plugins/vim-test.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"vim-test/vim-test",
-	config = function()
-		vim.keymap.set("n", "<leader>tf", ":TestFile<cr>")
-		vim.keymap.set("n", "<leader>tn", ":TestNearest<cr>")
-		vim.cmd("let test#strategy = 'wezterm'")
-	end,
-}
