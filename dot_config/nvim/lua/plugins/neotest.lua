89bea98fb552ac1613ca741af694f1aa11edcfbb
Yushi <yushi_61@proton.me>
2024-04-20 13:21:16 +0100

    feat: migrate to neotest

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+		"olimorris/neotest-rspec"
+  },
+	config = function ()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-rspec")
+			}
+		})
+
+		vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run()")
+		vim.keymap.set("n", "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))")
+	end
+}
