ff2af125994922cdc15bb03a41402a64af8816a0
Yushi <yushi_61@proton.me>
2024-05-07 20:24:50 +0100

    fix: keymaps for grapple

--- dot_config/nvim/lua/plugins/grapple.lua
+++ dot_config/nvim/lua/plugins/grapple.lua
@@ -4,8 +4,8 @@ return {
 		{ "nvim-tree/nvim-web-devicons", lazy = true }
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>m", require("grapple").toggle)
-		vim.keymap.set("n", "<leader>M", require("grapple").toggle_tags)
+		vim.keymap.set("n", "<leader>a", require("grapple").toggle)
+		vim.keymap.set("n", "<leader>e", require("grapple").toggle_tags)
 
 		vim.keymap.set("n", "<leader>&", "<cmd>Grapple select index=1<cr>")
 		vim.keymap.set("n", "<leader>é", "<cmd>Grapple select index=2<cr>")
