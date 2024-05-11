a0d9f97b594886c6cc4422a090828a08e36abedf
Yushi <yushi_61@proton.me>
2024-05-11 12:56:44 +0100

    feat: delete some keymaps

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -47,12 +47,3 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 
--- QOL:
--- Search centering
-vim.keymap.set('n', 'n', 'nzz')
-vim.keymap.set('n', 'N', 'Nzz')
--- Deleting to the void
-vim.keymap.set('n', 'x', '"_x')
-vim.keymap.set('v', 'x', '"_x')
--- Format pasted line
-vim.keymap.set('n', 'p', 'p==')
