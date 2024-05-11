a548b6e4c9d30e43cdbd18454a09af171332deae
Yushi <yushi_61@proton.me>
2024-05-11 12:56:53 +0100

    feat: add guicursor configuration option

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -74,3 +74,13 @@ vim.opt.updatetime = 50
 vim.opt.timeoutlen = 300
 vim.opt.wildmenu = true
 vim.opt.wrap = false
+
+-- guicursor options preference
+vim.opt.guicursor = {
+	"n-v-c:block", -- Normal, visual, command-line: block cursor
+	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
+	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
+	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
+	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+}
