44c8cb934cb2092a8842b876b10b8a8470ebf5ee
Yushi <yushi_61@proton.me>
2024-05-11 12:56:21 +0100

    feat: add spectre nvim

---
+++ dot_config/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-pack/nvim-spectre",
+	dependencies = {"nvim-lua/plenary.nvim"},
+	config = function ()
+		require("spectre").setup()
+
+		vim.keymap.set("n", "<leader>s", "<cmd>lua require('spectre').toggle()<cr>")
+	end
+}
