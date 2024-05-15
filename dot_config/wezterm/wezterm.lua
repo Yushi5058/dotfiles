1a2a4a7c56e7a332b14c2720d32592cbbb0d855a
Yushi <yushi_61@proton.me>
2024-05-15 15:48:34 +0100

    feat: migrate some files from catppuccin to rose-pine

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
 config.font = wezterm.font("MapleMono NF")
 config.font_size = 16.0
 
