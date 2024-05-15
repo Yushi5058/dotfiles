c69febbd02dd44f2d2b5da3d10d81b22f084986c
Yushi <yushi_61@proton.me>
2024-05-15 15:59:00 +0100

    fix: wezterm rose-pine theme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
+local theme = require('themes/rose_pine').main
 
 
 return {
-colors = theme.colors(),
+couors = theme.colors(),
 window_frame = theme.window_frame(), -- needed only if using fancy tab bar
 font = wezterm.font("MapleMono NF"),
 font_size = 16.0,
