c70b10893a96f1f78efc0ef21452959e4641a00f
Yushi <yushi_61@proton.me>
2024-05-15 16:00:01 +0100

    fix: typo

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local theme = require('themes/rose_pine').main
 
 
 return {
-couors = theme.colors(),
+colors = theme.colors(),
 window_frame = theme.window_frame(), -- needed only if using fancy tab bar
 font = wezterm.font("MapleMono NF"),
 font_size = 16.0,
