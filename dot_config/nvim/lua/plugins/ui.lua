40f6b1a78af76125120580b71c9e4de8690f2654
Yushi <yushi_61@proton.me>
2024-05-23 20:17:30 +0100

    feat: update fzf lua

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -114,7 +114,6 @@ return {
 		tag = "0.1.5",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			{
 				'nvim-telescope/telescope-fzf-native.nvim',
 
         -- `build` is used to run some command when the plugin is installed/updated.
@@ -138,13 +137,20 @@ return {
 			vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
 			vim.keymap.set("n", "fk", builtin.keymaps, {})
 
-			require("telescope").setup({
-				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown(),
-					},
+		require("telescope").setup({
+			extensions = {
+				['ui-select'] = {
+					require('telescope.themes').get_dropdown(),
 				},
-			})
+				fzf = {
+					fuzzy = true,                    -- false will only do exact matching
+					override_generic_sorter = true,  -- override the generic sorter
+					override_file_sorter = true,     -- override the file sorter
+					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				}
+			}
+		})
 
 			require('telescope').load_extension('fzf')
 			require("telescope").load_extension("ui-select")
