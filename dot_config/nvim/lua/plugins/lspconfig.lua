18d719281f727935795f80b710e82ed1c22ee9c5
Yushi <yushi_61@proton.me>
2024-05-25 21:25:34 +0100

    fix: lsp errors

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -56,18 +56,6 @@ return {
 				})
 			end,
 		})
-
-		-- Rubocop configuration
-		vim.opt.signcolumn = "yes"
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "ruby",
-			callback = function()
-				vim.lsp.start {
-					name = "rubocop",
-					cmd = { "bundle", "exec", "rubocop", "--lsp" },
-				}
-			end,
-		})
 	end,
 
 }
