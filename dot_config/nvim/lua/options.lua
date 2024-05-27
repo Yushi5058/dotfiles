692d8a37f048b925ead8f5adbc41b2b8e66da939
Yushi <yushi_61@proton.me>
2024-05-27 06:46:55 +0100

    feat: update paste and yank keymaps

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -34,7 +34,7 @@ vim.opt.smartindent = true
 vim.opt.incsearch = true
 
 -- More QOL options
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = ""
 vim.opt.encoding = "utf-8"
 
 -- Show which line your cursor is on
@@ -77,10 +77,10 @@ vim.opt.wrap = false
 
 -- guicursor options preference
 vim.opt.guicursor = {
-	"n-v-c:block", -- Normal, visual, command-line: block cursor
-	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
-	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
-	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
+	"n-v-c:block",                               -- Normal, visual, command-line: block cursor
+	"i-ci-ve:ver25",                             -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+	"r-cr:hor20",                                -- Replace, command-line replace: horizontal bar cursor with 20% height
+	"o:hor50",                                   -- Operator-pending: horizontal bar cursor with 50% height
+	"a:blinkwait700-blinkoff400-blinkon250",     -- All modes: blinking settings
 	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
