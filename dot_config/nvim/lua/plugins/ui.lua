1515649cdced24f5ccbde29a74d8380d267d8497
Yushi <yushi_61@proton.me>
2024-05-28 11:12:45 +0100

    feat: update trouble to dev branch beta version

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -13,8 +13,23 @@ return {
     },
     {
         "folke/trouble.nvim",
+        branch = "dev", -- IMPORTANT !
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
+        opts = {
+            {
+                modes = {
+                    test = {
+                        mode = "diagnostics",
+                        preview = {
+                            type = "split",
+                            relative = "win",
+                            position = "right",
+                            size = 0.3,
+                        },
+                    },
+                },
+            }
+        },
         keys = {
             { "<leader>xx", "<cmd>lua require('trouble').toggle()<cr>", mode = { "n" }, desc = "Trouble toggle" },
             {
@@ -56,7 +71,7 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
-        dependencies = {"nvim-lua/plenary.nvim"},
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
@@ -72,8 +87,8 @@ return {
     {
         'stevearc/oil.nvim',
         config = function()
-            vim.keymap.set('n', '<leader>n',  function()
-            require("oil").toggle_float()
+            vim.keymap.set('n', '<leader>n', function()
+                require("oil").toggle_float()
             end)
             require('oil').setup({
                 default_file_explorer = true,
