6dc341d002a14e1d1c0ef7554e8f4ab8eac7e423
Yushi <yushi_61@proton.me>
2024-05-29 13:08:58 +0100

    feat: update keymaps

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -3,7 +3,7 @@ return {
 	{
 		"ellisonleao/glow.nvim",
 		keys = {
-			{ "<leader>g", "<cmd>Glow<cr>", mode = { "n" }, desc = "Markdown preview" },
+			{ "<leader>gl", "<cmd>Glow<cr>", mode = { "n" }, desc = "Markdown preview" },
 		},
 		config = function()
 			require("glow").setup()
