87effc2f089334c5ea1ecff56e5f6e1638a3c6f4
Yushi <yushi_61@proton.me>
2024-05-29 13:30:08 +0100

    feat: update git nvim plugins

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,29 +1,28 @@
-return 	{
-		"echasnovski/mini.nvim",
-		version = false,
-		lazy = false,
-		config = function()
-			require("mini.cursorword").setup()
-			require("mini.indentscope").setup()
-			require("mini.pairs").setup()
-			require("mini.surround").setup()
-			-- NOTE:
-			-- add = 'sa', -- Add surrounding in Normal and Visual modes
-			--  delete = 'sd', -- Delete surrounding
-			--  find = 'sf', -- Find surrounding (to the right)
-			--  find_left = 'sF', -- Find surrounding (to the left)
-			--  highlight = 'sh', -- Highlight surrounding
-			--  replace = 'sr', -- Replace surrounding
-			require("mini.animate").setup()
-			require("mini.comment").setup()
-			-- NOTE:
-			-- on visual mode :
-			-- gc to comment
-			-- gcc to comment current line
-			require("mini.statusline").setup()
-			require("mini.starter").setup()
-			require("mini.notify").setup()
-
-        end,
-    }
-
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	lazy = false,
+	config = function()
+		require("mini.cursorword").setup()
+		require("mini.indentscope").setup()
+		require("mini.pairs").setup()
+		require("mini.surround").setup()
+		-- NOTE:
+		-- add = 'sa', -- Add surrounding in Normal and Visual modes
+		--  delete = 'sd', -- Delete surrounding
+		--  find = 'sf', -- Find surrounding (to the right)
+		--  find_left = 'sF', -- Find surrounding (to the left)
+		--  highlight = 'sh', -- Highlight surrounding
+		--  replace = 'sr', -- Replace surrounding
+		require("mini.animate").setup()
+		require("mini.comment").setup()
+		-- NOTE:
+		-- on visual mode :
+		-- gc to comment
+		-- gcc to comment current line
+		require("mini.statusline").setup()
+		require("mini.starter").setup()
+		require("mini.notify").setup()
+		require("mini.diff").setup()
+	end,
+}
