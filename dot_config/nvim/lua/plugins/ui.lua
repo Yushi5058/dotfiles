c8350156dbc76913fa735ceddbdb373c7e86d3a2
Yushi <yushi_61@proton.me>
2024-05-30 14:31:51 +0100

    feat: merge to trouble v3 plugin

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -13,7 +13,6 @@ return {
     },
     {
         "folke/trouble.nvim",
-        branch = "dev", -- IMPORTANT !
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
             {
