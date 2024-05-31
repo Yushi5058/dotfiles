494434fe351dd2ac3fff14526606bd744964235f
Yushi <yushi_61@proton.me>
2024-05-31 17:23:26 +0100

    feat: add todo telescope integration

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -72,6 +72,7 @@ return {
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fc", "<cmd>TodoTelescope<cr>")
             vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
         end,
     },
