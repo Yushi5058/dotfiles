a2a6235804cd427c6ee7ea43ee3d4314e7cc8981
Yushi <yushi_61@proton.me>
2024-06-03 16:12:23 +0100

    feat: add prettier formatter for js

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { "eslint" },
+				javascript = { "eslint", "prettier" },
 				ruby = { "rubocop" },
 				markdown = { "prettier" },
 				css = { "prettier" },
