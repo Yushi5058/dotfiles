0467551f62dda68a3a614f2ebf825606ba854603
Yushi <yushi_61@proton.me>
2024-06-04 12:01:45 +0100

    feat: update grapple keymaps

--- dot_config/nvim/lua/plugins/grapple.lua
+++ dot_config/nvim/lua/plugins/grapple.lua
@@ -7,8 +7,8 @@ return {
 		vim.keymap.set("n", "<leader>a", require("grapple").toggle)
 		vim.keymap.set("n", "<c-e>", require("grapple").toggle_tags)
 
-		vim.keymap.set("n", "<C-&>", "<cmd>Grapple select index=1<cr>")
-		vim.keymap.set("n", "<C-é>", "<cmd>Grapple select index=2<cr>")
-		vim.keymap.set("n", "<C-\">", "<cmd>Grapple select index=3<cr>")
+		vim.keymap.set("n", "<leader>&", "<cmd>Grapple select index=1<cr>")
+		vim.keymap.set("n", "<leader>é", "<cmd>Grapple select index=2<cr>")
+		vim.keymap.set("n", "<leader>\"", "<cmd>Grapple select index=3<cr>")
 	end
 }
