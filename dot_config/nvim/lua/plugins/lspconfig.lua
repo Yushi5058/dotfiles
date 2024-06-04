030ffb33a1d510ef31be024c326068a6b5b1e267
Yushi <yushi_61@proton.me>
2024-06-04 14:23:08 +0100

    feat: remove gdscript (not working)

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -36,12 +36,7 @@ return {
 			jsonls = {},
 			bashls = {},
 			clangd = {},
-			gdtoolkit = {
-				force_setup = true,
-				single_file_support = false,
-				root_dir = require('lspconfig.util').root_pattern('project.godot', '.git'),
-				filetypes = { 'gd', 'gdscript', 'gdscript3' }
-			}
+
 		}
 
 		require("mason").setup()
