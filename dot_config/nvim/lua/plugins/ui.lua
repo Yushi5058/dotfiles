390ba74fc8204e6b69a5444a26da7aa252a73038
Yushi <yushi_61@proton.me>
2024-06-06 15:11:13 +0100

    feat: add rose-pine bat theme

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -118,6 +118,6 @@ return {
     {
         "lukas-reineke/headlines.nvim",
         dependencies = "nvim-treesitter/nvim-treesitter",
-        config = true,     -- or `opts = {}`
+        config = true, -- or `opts = {}`
     },
 }
