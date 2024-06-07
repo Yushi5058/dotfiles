53482b34f227c1a7f049dd6bc07eadc4e8b71e0a
Yushi <yushi_61@proton.me>
2024-06-08 00:23:19 +0100

    feat: update nvim config

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -11,9 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
 
 local opts = {
 	install = {
