6b16101883f207201ec07c7ebbf068d00c2fe086
Yushi <yushi_61@proton.me>
2024-06-08 00:20:18 +0100

    fix: errors on lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -11,9 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("options")
-require("keymaps")
-require("autocmds")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
 
 local opts = {
 	install = {
