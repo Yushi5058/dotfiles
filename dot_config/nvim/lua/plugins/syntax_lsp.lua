5fc5140c84b4e9f9c37479bdb5f290768cb0b661
Yushi <yushi_61@proton.me>
2024-06-16 12:38:20 +0100

    feat: update nvim config to add java and kotlin development

--- dot_config/nvim/lua/plugins/syntax_lsp.lua
+++ dot_config/nvim/lua/plugins/syntax_lsp.lua
@@ -41,9 +41,10 @@ return {
 				rubocop = {},
 				pyright = {},
 				kotlin_language_server = {},
-				groovyls = {}
+				jdtls = {}
 			}
 
+			require("java").setup()
 			require("mason").setup()
 			require("mason-lspconfig").setup({
 				ensure_installed = vim.tbl_keys(servers),
@@ -140,4 +141,26 @@ return {
 			vim.keymap.set("n", "<leader>ta", "<cmd>AerialToggle! right<CR>")
 		end,
 	},
+	{
+		'nvim-java/nvim-java',
+		dependencies = {
+			'nvim-java/lua-async-await',
+			'nvim-java/nvim-java-refactor',
+			'nvim-java/nvim-java-core',
+			'nvim-java/nvim-java-test',
+			'nvim-java/nvim-java-dap',
+			'MunifTanjim/nui.nvim',
+			'neovim/nvim-lspconfig',
+			'mfussenegger/nvim-dap',
+			{
+				'williamboman/mason.nvim',
+				opts = {
+					registries = {
+						'github:nvim-java/mason-registry',
+						'github:mason-org/mason-registry',
+					},
+				},
+			}
+		},
+	}
 }
