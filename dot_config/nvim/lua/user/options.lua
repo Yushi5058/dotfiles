d4f1ad80c19fb31fcb0b331698cef797834e337d
Yushi <yushi_61@proton.me>
2024-06-21 17:09:53 +0100

    feat: migrate from oil to netrw

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -86,5 +86,4 @@ vim.opt.guicursor = {
 vim.g.netrw_keepdir = 0
 vim.g.netrw_winsize = 30
 vim.g.netrw_banner = 0
-vim.g.netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'
 vim.g.netrw_localcopydircmd = 'cp -r'
