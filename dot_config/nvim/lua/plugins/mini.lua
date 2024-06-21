ad7d8ff12818e3875fe855ec790c630cbcf8b6a2
Yushi <yushi_61@proton.me>
2024-06-21 22:02:00 +0100

    feat: add mini.starter to plugins

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,18 +1,19 @@
 return {
-	"echasnovski/mini.nvim",
-	version = false,
-	lazy = false,
-	config = function()
-		require("mini.cursorword").setup()
-		require("mini.indentscope").setup()
-		require("mini.pairs").setup()
-		require("mini.comment").setup()
-		require("mini.diff").setup()
-		require("mini.git").setup()
-		require("mini.completion").setup()
-		require("mini.notify").setup()
-		require("mini.statusline").setup()
-		require("mini.surround").setup()
-		require("mini.animate").setup()
-	end,
+  "echasnovski/mini.nvim",
+  version = false,
+  lazy = false,
+  config = function()
+    require("mini.cursorword").setup()
+    require("mini.indentscope").setup()
+    require("mini.pairs").setup()
+    require("mini.comment").setup()
+    require("mini.diff").setup()
+    require("mini.git").setup()
+    require("mini.completion").setup()
+    require("mini.notify").setup()
+    require("mini.statusline").setup()
+    require("mini.surround").setup()
+    require("mini.animate").setup()
+    require("mini.starter").setup()
+  end,
 }
