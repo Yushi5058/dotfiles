e81ebebf6ccdf4357c137d683e2b0bfd2fba4ec2
Yushi <yushi_61@proton.me>
2024-06-22 16:08:15 +0100

    fix: remove cwd option from FzfLua files picker

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       -- calling `setup` is optional for customization
       require("fzf-lua").setup({})
-      vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files({ cwd='~/<folder>' })<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
       vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<cr>")
       vim.keymap.set("n", "<leader>fh", "<cmd>lua require('fzf-lua').helptags()<cr>")
       vim.keymap.set("n", "<leader>fd", "<cmd>lua require('fzf-lua').diagnostics_workspace()<cr>")
