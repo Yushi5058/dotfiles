35a036f094d3ff6a29a1c82e58ee40a88fa166e6
Yushi <yushi_61@proton.me>
2024-06-22 23:37:41 +0100

    feat: change colorscheme to habamax

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -9,6 +9,9 @@ vim.g.have_nerd_font = true
 -- Disable mouse
 vim.opt.mouse = ""
 
+-- Set habamax colorscheme
+vim.cmd [[colorscheme habamax]]
+
 -- Line numbers as default
 -- Relative line numbers for jumping
 vim.opt.nu = true
@@ -74,12 +77,12 @@ vim.opt.wrap = false
 
 -- guicursor options preference
 vim.opt.guicursor = {
-    "n-v-c:block",                                  -- Normal, visual, command-line: block cursor
-    "i-ci-ve:ver25",                                -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-    "r-cr:hor20",                                   -- Replace, command-line replace: horizontal bar cursor with 20% height
-    "o:hor50",                                      -- Operator-pending: horizontal bar cursor with 50% height
-    "a:blinkwait700-blinkoff400-blinkon250",        -- All modes: blinking settings
-    "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+  "n-v-c:block",                                    -- Normal, visual, command-line: block cursor
+  "i-ci-ve:ver25",                                  -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+  "r-cr:hor20",                                     -- Replace, command-line replace: horizontal bar cursor with 20% height
+  "o:hor50",                                        -- Operator-pending: horizontal bar cursor with 50% height
+  "a:blinkwait700-blinkoff400-blinkon250",          -- All modes: blinking settings
+  "sm:block-blinkwait175-blinkoff150-blinkon175",   -- Showmatch: block cursor with specific blinking settings
 }
 
 -- netrw settings
