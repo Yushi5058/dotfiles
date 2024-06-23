f97ac5e62e7d36b8c07cf4b396810787acb6b506
Yushi <yushi_61@proton.me>
2024-06-23 18:20:30 +0100

    feat: update neovim configuration

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -1,7 +1,3 @@
--- Set space as the leader key
--- NOTE: Must happen before plugins are loaded
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
