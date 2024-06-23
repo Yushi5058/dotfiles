c100e2cb216cfa6654ebfd6b2bd7103135658f9d
Yushi <yushi_61@proton.me>
2024-06-23 01:02:43 +0100

    feat: update keymaps

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -1,79 +1,75 @@
-local keymap = vim.keymap
+-- helper function
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_keymap(mode, lhs, rhs, options)
+end
+
 -- disable space in normal mode for leader key
-keymap.set({ "n", "v" }, "<space>", "<nop>")
+map({ "n", "v" }, "<space>", "<nop>")
 
 -- Move line up/down
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
-keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
-keymap.set("n", "<leader>m", "<cmd>Mason<cr>")
+map("n", "<leader>l", "<cmd>Lazy<cr>")
+map("n", "<leader>m", "<cmd>Mason<cr>")
 
 -- dealing with word wrap
-keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- lsp keymaps
-keymap.set("n", "K", vim.lsp.buf.hover)
-keymap.set("n", "<leader>k", vim.lsp.buf.signature_help)
-keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-keymap.set("n", "<leader>gr", vim.lsp.buf.references)
-keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+-- lsp maps
+map("n", "K", vim.lsp.buf.hover)
+map("n", "<leader>k", vim.lsp.buf.signature_help)
+map("n", "<leader>gd", vim.lsp.buf.definition)
+map("n", "<leader>gr", vim.lsp.buf.references)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-keymap.set("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-keymap.set("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
-keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 
 -- escape search highlight / insert mode
-keymap.set("n", "jk", "<cmd>nohlsearch<cr>")
-keymap.set({ "i", "v" }, "jk", "<Esc>")
+map("n", "jk", "<cmd>nohlsearch<cr>")
+map({ "i", "v" }, "jk", "<Esc>")
 
--- Yank one word to the clipboard
-keymap.set("n", "<leader>yw", '"+yiw', { noremap = true, silent = true })
-keymap.set("v", "<leader>yw", '"+y', { noremap = true, silent = true })
-
--- Yank entire line to the clipboard
-keymap.set("n", "<leader>Y", '"+yy', { noremap = true, silent = true })
-keymap.set("v", "<leader>Y", '"+y', { noremap = true, silent = true })
+-- Yank to the clipboard
+map({ 'n', 'v' }, '<leader>y', '"+y', { silent = true })
+map({ 'n', 'v' }, '<leader>Y', '"+Y', { silent = true })
 
 -- Paste from the clipboard
-keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
-keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })
-keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })
-keymap.set("v", "<leader>P", '"+P', { noremap = true, silent = true })
-
--- Replace selection with clipboard contents
-keymap.set("v", "<leader>r", '"_d"+P', { noremap = true, silent = true })
-
--- Copy file path to clipboard
-keymap.set("n", "<leader>cf", ':let @+=expand("%:p")<CR>', { noremap = true, silent = true })
+map({ 'n', 'v' }, '<leader>p', '"+p', { silent = true })
+map({ 'n', 'v' }, '<leader>P', '"+P', { silent = true })
 
 -- Delete line to the void register
-keymap.set("n", "<leader>dd", '"_dd', { noremap = true, silent = true })
+map({ 'n', 'v' }, '<leader>d', '"_d', { silent = true })
 
 -- Keymaps for neovim splits
-keymap.set("n", "sv", "<cmd>vsplit<cr>")
-keymap.set("n", "sh", "<cmd>split<cr>")
+map("n", "sv", "<cmd>vsplit<cr>")
+map("n", "sh", "<cmd>split<cr>")
 
 -- navigate between splits
-keymap.set("n", "<C-h", "<C-w>h")
-keymap.set("n", "<C-l", "<C-w>l")
-keymap.set("n", "<C-k", "<C-w>k")
-keymap.set("n", "<C-j", "<C-w>j")
+map("n", "<C-h", "<C-w>h")
+map("n", "<C-l", "<C-w>l")
+map("n", "<C-k", "<C-w>k")
+map("n", "<C-j", "<C-w>j")
 -- resize splits
-vim.keymap.set("n", "<A-h>", "10<C-w><", { noremap = true, silent = true })
-vim.keymap.set("n", "<A-l>", "10<C-w>>", { noremap = true, silent = true })
-vim.keymap.set("n", "<A-j>", "10<C-w>+", { noremap = true, silent = true })
-vim.keymap.set("n", "<A-k>", "10<C-w>-", { noremap = true, silent = true })
+map("n", "<A-h>", "10<C-w><", { noremap = true, silent = true })
+map("n", "<A-l>", "10<C-w>>", { noremap = true, silent = true })
+map("n", "<A-j>", "10<C-w>+", { noremap = true, silent = true })
+map("n", "<A-k>", "10<C-w>-", { noremap = true, silent = true })
 -- netrw explorer
-vim.keymap.set("n", "<leader>n", "<cmd>Ex<cr>")
+map("n", "<leader>n", "<cmd>Ex<cr>")
