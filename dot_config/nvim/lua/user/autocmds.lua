81e1785d9f5f1cf111e0ef82dced08734a5f4fab
Yushi <yushi_61@proton.me>
2024-06-23 21:31:34 +0100

    fix: lsp formatting with formatters

--- dot_config/nvim/lua/user/autocmds.lua
+++ dot_config/nvim/lua/user/autocmds.lua
@@ -31,24 +31,38 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Define commands per filetype
+local commands = {
+  html = "prettier --write",
+  css = "prettier --write",
+  markdown = "prettier --write",
+  javascript = "eslint --fix",
+  ruby = "rubocop -A",
+  lua = "stylua",
+  c = "clang-format -i",
+  cpp = "clang-format -i",
+}
+
 -- autoformat on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local filepath = vim.api.nvim_buf_get_name(bufnr)
     if vim.bo.filetype ~= "gitcommit" then
-      if vim.bo.filetype == "css" or vim.bo.filetype == "markdown" or vim.bo.filetype == "html" then
-        vim.fn.system("prettier --write " .. vim.fn.shellescape(filepath))
-      elseif vim.bo.filetype == "javascript" then
-        vim.fn.system("eslint --fix " .. vim.fn.shellescape(filepath))
-      elseif vim.bo.filetype == "ruby" then
-        vim.fn.system("rubocop -A " .. vim.fn.shellescape(filepath))
-      elseif vim.bo.filetype == "lua" then
-        vim.fn.system("stylua " .. vim.fn.shellescape(filepath))
-      elseif vim.bo.filetype == "C" or vim.bo.filetype == "cpp" then
-        vim.fn.system("clang-format -i " .. vim.fn.shellescape(filepath))
-      else
+      local ft = vim.bo.filetype
+      local command = commands[ft]
+
+      if command then
+          local range = {1, vim.fn.line("$")}  -- Format entire buffer
+          local output = vim.fn.systemlist(command, {
+              args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+              capture_output = true,
+              input = table.concat(vim.api.nvim_buf_get_lines(0, range[1] - 1, range[2], false), "\n"),
+          })
+
+        if output and #output > 0 then
+          vim.api.nvim_buf_set_lines(0, range[1] - 1, range[2], false, output)
+        end
+             else
         vim.lsp.buf.format({ async = true })
       end
     end
