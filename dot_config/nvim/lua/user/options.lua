651338b7beab38c5d6e836767a84d10d1e8947e9
Yushi <yushi_61@proton.me>
2024-06-23 16:09:28 +0100

    feat: change colorscheme to sorbet

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -10,7 +10,7 @@ vim.g.have_nerd_font = true
 vim.opt.mouse = ""
 
 -- Set habamax colorscheme
-vim.cmd [[colorscheme habamax]]
+vim.cmd [[colorscheme sorbet]]
 
 -- Line numbers as default
 -- Relative line numbers for jumping
@@ -77,12 +77,12 @@ vim.opt.wrap = false
 
 -- guicursor options preference
 vim.opt.guicursor = {
-  "n-v-c:block",                                    -- Normal, visual, command-line: block cursor
-  "i-ci-ve:ver25",                                  -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-  "r-cr:hor20",                                     -- Replace, command-line replace: horizontal bar cursor with 20% height
-  "o:hor50",                                        -- Operator-pending: horizontal bar cursor with 50% height
-  "a:blinkwait700-blinkoff400-blinkon250",          -- All modes: blinking settings
-  "sm:block-blinkwait175-blinkoff150-blinkon175",   -- Showmatch: block cursor with specific blinking settings
+  "n-v-c:block",                                  -- Normal, visual, command-line: block cursor
+  "i-ci-ve:ver25",                                -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+  "r-cr:hor20",                                   -- Replace, command-line replace: horizontal bar cursor with 20% height
+  "o:hor50",                                      -- Operator-pending: horizontal bar cursor with 50% height
+  "a:blinkwait700-blinkoff400-blinkon250",        -- All modes: blinking settings
+  "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
 
 -- netrw settings
