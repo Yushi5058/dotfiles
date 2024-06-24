4ef70709f132c45cbc7c4941f67426f9d4f5f7e2
Yushi <yushi_61@proton.me>
2024-06-24 14:23:43 +0100

    fix: rose-pine colorscheme typo

--- dot_config/nvim/lua/user/deps.lua
+++ dot_config/nvim/lua/user/deps.lua
@@ -17,7 +17,7 @@ require('mini.deps').setup({ path = { package = path_package } })
 
 local add = MiniDeps.add
 add('rose-pine/neovim')
-vim.cmd('colorscheme rose-pine main')
+vim.cmd('colorscheme rose-pine-main')
 add('ibhagwan/fzf-lua')
 add('wakatime/vim-wakatime')
 add({
