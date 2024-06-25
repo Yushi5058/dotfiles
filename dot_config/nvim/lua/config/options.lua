556a297715827d21252e995919d5d867caa69714
Yushi <yushi_61@proton.me>
2024-06-26 00:04:01 +0100

    feat: remove opt confirm

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,4 @@
 vim.opt.mouse = "a"
-vim.opt.confirm = true
 vim.opt.cursorline = false
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -35,10 +34,10 @@ vim.opt.wrap = false
 
 -- guicursor options preference
 vim.opt.guicursor = {
-	"n-v-c:block", -- Normal, visual, command-line: block cursor
-	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
-	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
-	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
-	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
+    "n-v-c:block",                               -- Normal, visual, command-line: block cursor
+    "i-ci-ve:ver25",                             -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+    "r-cr:hor20",                                -- Replace, command-line replace: horizontal bar cursor with 20% height
+    "o:hor50",                                   -- Operator-pending: horizontal bar cursor with 50% height
+    "a:blinkwait700-blinkoff400-blinkon250",     -- All modes: blinking settings
+    "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
