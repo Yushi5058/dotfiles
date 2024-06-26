5be7bfd31f71aa8a4db36edcc06838b7c51d9a83
Yushi <yushi_61@proton.me>
2024-06-26 19:41:32 +0100

    feat: migrate some plugins to mini.lib

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -4,5 +4,7 @@ return {
     config = function()
         require("mini.statusline").setup({use_icons = vim.g.have_nerd_font})
         require("mini.completion").setup()
+        require("mini.git").setup()
+        require("mini.diff").setup()
     end
 }
