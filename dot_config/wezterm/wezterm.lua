1c907bd37bdb40519d198aea275356db79f47272
Yushi <yushi_61@proton.me>
2024-06-26 01:36:37 +0100

    feat: update wezterm colorscheme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
--- local theme = require('themes/kanagawa_paper')
 
 
 return {
     -- colors = theme.colors(),
     font = wezterm.font("MapleMono NF"),
+    color_scheme = "N0Tch2K",
     enable_tab_bar = false,
     font_size = 18.0,
 }
