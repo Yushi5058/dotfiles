2149e30536aab055c6191d657e16f12e66411c09
Yushi <yushi_61@proton.me>
2024-06-26 19:56:23 +0100

    feat: update statusline configuration

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,17 @@
 return {
-    "echasnovski/mini.nvim",
-    version = "*",
-    config = function()
-        require("mini.statusline").setup({use_icons = vim.g.have_nerd_font})
-        require("mini.completion").setup()
-        require("mini.git").setup()
-        require("mini.diff").setup()
-    end
+	"echasnovski/mini.nvim",
+	version = "*",
+	config = function()
+		require("mini.statusline").setup({ use_icons = vim.g.have_nerd_font })
+		-- You can configure sections in the statusline by overriding their
+		-- default behavior. For example, here we set the section for
+		-- cursor location to LINE:COLUMN
+		---@diagnostic disable-next-line: duplicate-set-field
+		require("mini.statusline").section_location = function()
+			return "%2l:%-2v"
+		end
+		require("mini.completion").setup()
+		require("mini.git").setup()
+		require("mini.diff").setup()
+	end,
 }
