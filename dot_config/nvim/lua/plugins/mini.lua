5230228910bd23b9fbb9d25c92260368e4dd5c11
Yushi <yushi_61@proton.me>
2024-06-26 19:34:45 +0100

    feat: add nerdfont support for icons

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
-        require("mini.statusline").setup()
+        require("mini.statusline").setup({use_icons = vim.g.have_nerd_font})
         require("mini.completion").setup()
     end
 }
