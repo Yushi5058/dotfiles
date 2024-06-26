1ad3ef70afcde3b90c2eb6b7462831250624a4d4
Yushi <yushi_61@proton.me>
2024-06-26 02:04:19 +0100

    fix: font typo

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Kanagawa (Gogh)'
-config.font = "MapleMono NF"
+config.font = wezterm.font "MapleMono NF"
 config.enable_tab_bar = false
 config.font_size = 20
 config.line_height = 1.5
