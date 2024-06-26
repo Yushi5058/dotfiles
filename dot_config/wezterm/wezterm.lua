486bf85fb38d428b570a7ed1684b992c6b3ba0f1
Yushi <yushi_61@proton.me>
2024-06-26 01:53:39 +0100

    feat: update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.colorscheme = "N0Tch2K"
+config.font = "MapleMono NF"
+config.enable_tab_bar = false
+config.font_size = 20
+config.line_height = 1.5
 
 
-return {
-    -- colors = theme.colors(),
-    font = wezterm.font("MapleMono NF"),
-    color_scheme = "N0Tch2K",
-    enable_tab_bar = false,
-    font_size = 18.0,
-}
