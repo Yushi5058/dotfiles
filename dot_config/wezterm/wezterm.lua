177408d5f08d868c9d46e7cebbf35e4593cfef10
Yushi <yushi_61@proton.me>
2024-06-26 01:29:56 +0100

    feat: remove rose-pine theme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,9 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-local theme = require('themes/rose_pine').main
+-- local theme = require('themes/kanagawa_paper')
 
 
 return {
-    colors = theme.colors(),
+    -- colors = theme.colors(),
     font = wezterm.font("MapleMono NF"),
     enable_tab_bar = false,
     font_size = 18.0,
