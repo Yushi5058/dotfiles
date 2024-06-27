6a965c813871347f37c901db57c8b597d9d0c2c9
Yushi <yushi_61@proton.me>
2024-06-27 17:05:42 +0100

    feat: add expressoft colorscheme

---
+++ dot_config/nvim/lua/plugins/expressoft.lua
@@ -0,0 +1,11 @@
+return {
+    "RRethy/base16-nvim",
+    config = function()
+        require('base16-colorscheme').setup({
+    base00 = '#120d09', base01 = '#241b13', base02 = '#372b20', base03 = '#4b3c2e',
+    base04 = '#766454', base05 = '#8f7c6d', base06 = '#aa998b', base07 = '#cabeb3',
+    base08 = '#44739c', base09 = '#44479c', base0A = '#6d449c', base0B = '#9c4473',
+    base0C = '#9c6d44', base0D = '#739c44', base0E = '#449c6d', base0F = '#449c99',
+})
+end,
+}
