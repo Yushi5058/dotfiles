74e376771d747031f414d0472308a61451043131
Yushi <yushi_61@proton.me>
2024-06-28 12:30:46 +0100

    feat: update wezterm colorscheme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,22 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+return {
+	color_scheme = "expressoft",
+	color_schemes = {
+		["expressoft"] = {
+			foreground = "#aa998b",
+			background = "#120d09",
+			cursor_bg = "#cabeb3",
+			cursor_border = "#766454",
+			cursor_fg = "#241b13",
+			selection_bg = "#766454",
+			selection_fg = "#8f7c6d",
 
-config.color_scheme = 'Kanagawa (Gogh)'
-config.font = wezterm.font "MapleMono NF"
-config.enable_tab_bar = false
-config.font_size = 20
-config.line_height = 1.5
-
-return config
+			ansi = { "#120d09", "#9c4473", "#739c44", "#9c6d44", "#44739c", "#6d449c", "#449c6d", "#4b3c2e" },
+			brights = {  "#120d09", "#9c4473", "#739c44", "#9c6d44", "#44739c", "#6d449c", "#449c6d", "#4b3c2e"},
+		},
+	},
+	font = wezterm.font("MapleMono NF"),
+	enable_tab_bar = false,
+	font_size = 20,
+	line_height = 1.5,
+}
