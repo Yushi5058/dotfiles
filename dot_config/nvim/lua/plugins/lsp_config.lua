5e4433c15cbc74db72a01a0705eb936a4569de0b
Yushi <yushi_61@proton.me>
2024-06-29 13:31:53 +0100

    feat: update lsp config

--- dot_config/nvim/lua/plugins/lsp_config.lua
+++ dot_config/nvim/lua/plugins/lsp_config.lua
@@ -1,57 +1,22 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		local servers = {
-			"lua_ls",
-			"tsserver",
-			"ruby_lsp",
-			"html",
-			"cssls",
-			"jsonls",
-			"clangd",
-			"emmet_ls",
-		}
-		for _, server in ipairs(servers) do
-			lspconfig[server].setup({
-				capabilities = capabilities,
-			})
-		end
-
-		lspconfig.html.setup({
-			filetypes = { "erb", "eruby" },
-		})
-		lspconfig.lua_ls.setup({
-			on_init = function(client)
-				local path = client.workspace_folders[1].name
-				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-					return
-				end
-
-				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						-- Tell the language server which version of Lua you're using
-						-- (most likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-					},
-					-- Make the server aware of Neovim runtime files
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME,
-							-- Depending on the usage, you might want to add additional paths here.
-							-- "${3rd}/luv/library"
-							-- "${3rd}/busted/library",
-						},
-						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-						-- library = vim.api.nvim_get_runtime_file("", true)
-					},
-				})
-			end,
-			settings = {
-				Lua = {},
-			},
-		})
-	end,
-}
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+            lspconfig.lua_ls.setup{
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                    }}
+                }
+                lspconfig.tsserver.setup{}
+                lspconfig.ruby_lsp.setup{}
+                lspconfig.html.setup{}
+                lspconfig.cssls.setup{}
+                lspconfig.jsonls.setup{}
+                lspconfig.clangd.setup{}
+                lspconfig.emmet_ls.setup{}
+        end
+    }
