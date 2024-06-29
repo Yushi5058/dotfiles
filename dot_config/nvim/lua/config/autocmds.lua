b84aa93dc1de18301334e84571cdeb4c1305d724
Yushi <yushi_61@proton.me>
2024-06-29 13:31:04 +0100

    feat: add terminal config

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,11 +1,14 @@
+-- oil configuration
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "oil",
     callback = function()
         vim.opt.relativenumber = false
         vim.opt.number = false
+        vim.opt.signcolumn = "no"
     end,
 })
 
+-- text yanking highlight
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
     pattern = "*",
@@ -15,6 +18,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+-- spell suggestions
 vim.api.nvim_create_autocmd({ "FileType" }, {
     group = vim.api.nvim_create_augroup("edit_text", { clear = true }),
     pattern = { "gitcommit", "markdown", "txt" },
@@ -24,3 +28,30 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
         vim.opt_local.spell = true
     end,
 })
+
+-- terminal configuration
+local set = vim.opt_local
+
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+
+    vim.bo.filetype = "terminal"
+  end,
+})
+
+-- Easily hit escape in terminal mode.
+vim.keymap.set("t", "<C-j>", "<c-\\><c-n>")
+
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", "<leader>st", function()
+    vim.cmd.new()
+    vim.cmd.wincmd "J"
+    vim.api.nvim_win_set_height(0, 12)
+    vim.wo.winfixheight = true
+    vim.cmd.term()
+end)
