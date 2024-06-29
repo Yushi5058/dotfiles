a1fab8dc2ea4c1a1de3cac055cadda9e837a2835
Yushi <yushi_61@proton.me>
2024-06-29 13:38:39 +0100

    fix: remove config and change it to opts

--- dot_config/nvim/lua/plugins/undotree.lua
+++ dot_config/nvim/lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
-  config = true,
+  opts = {},
   keys = { -- load the plugin only when using it's keybinding:
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
   },
