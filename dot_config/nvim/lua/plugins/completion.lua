596cac4a6e607a9c82e4ccecde9e0d7327437667
Yushi <yushi_61@proton.me>
2024-07-02 10:39:14 +0100

    feat: update nvim config

---
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-c>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          { name = "path" }, -- path files
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+}
--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,18 +1,16 @@
 return {
-	"echasnovski/mini.nvim",
-	version = "*",
-	config = function()
-        -- use icons
-		require("mini.statusline").setup({ use_icons = vim.g.have_nerd_font })
-		-- cursor location to LINE:COLUMN
-		require("mini.statusline").section_location = function()
-			return "%2l:%-2v"
-		end
-		require("mini.completion").setup()
-		require("mini.git").setup()
-		require("mini.diff").setup()
-		require("mini.notify").setup()
+    "echasnovski/mini.nvim",
+    version = "*",
+    config = function()
+        -- cursor location to LINE:COLUMN
+        require("mini.statusline").setup({
+            section_location = "%2l:%-2v"
+        })
+        require("mini.git").setup()
+        require("mini.surround").setup()
+        require("mini.diff").setup()
+        require("mini.notify").setup()
         -- use mini.notify as default notifier
-		vim.notify = require("mini.notify").make_notify()
-	end,
+        vim.notify = require("mini.notify").make_notify()
+    end,
 }
