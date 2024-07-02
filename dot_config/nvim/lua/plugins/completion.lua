1237496da2ade6b4444a20698f26f801d6f3bd01
Yushi <yushi_61@proton.me>
2024-07-02 10:48:13 +0100

    feat: update cmp sources

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -2,6 +2,8 @@ return {
   {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-buffer",
   },
   {
     "L3MON4D3/LuaSnip",
@@ -17,30 +19,46 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-c>"] = cmp.mapping.abort(),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          { name = "path" }, -- path files
-        }, {
-          { name = "buffer" },
-        }),
+          snippet = {
+              expand = function(args)
+                  require("luasnip").lsp_expand(args.body)
+              end,
+          },
+          window = {
+              completion = cmp.config.window.bordered(),
+              documentation = cmp.config.window.bordered(),
+          },
+          mapping = cmp.mapping.preset.insert({
+              ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+              ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+              ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+              ["<C-d>"] = cmp.mapping.scroll_docs(4),
+              ["<C-Space>"] = cmp.mapping.complete(),
+              ["<C-c>"] = cmp.mapping.abort(),
+              ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          }),
+          sources = cmp.config.sources({
+              { name = "nvim_lsp" },
+              { name = "luasnip", max_item_count = 3 }, -- snippets
+              { name = "buffer", max_item_count = 5 }, -- text within current buffer
+              { name = "path", max_item_count = 3 },
+          }),
       })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' }
+          }
+        }
+      })
+    })
     end,
   },
 }
