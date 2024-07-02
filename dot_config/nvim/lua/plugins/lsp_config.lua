bfad6122955b5dfee1213245fa1f694344cac2e2
Yushi <yushi_61@proton.me>
2024-07-02 10:39:14 +0100

    feat: update nvim config

--- dot_config/nvim/lua/plugins/lsp_config.lua
+++ dot_config/nvim/lua/plugins/lsp_config.lua
@@ -3,20 +3,20 @@ return {
     config = function()
         local lspconfig = require("lspconfig")
         local capabilities = vim.lsp.protocol.make_client_capabilities()
-            lspconfig.lua_ls.setup{
-                settings = {
-                    Lua = {
-                        completion = {
-                            callSnippet = "Replace",
-                        },
-                    }}
-                }
-                lspconfig.tsserver.setup{}
-                lspconfig.ruby_lsp.setup{}
-                lspconfig.html.setup{}
-                lspconfig.cssls.setup{}
-                lspconfig.jsonls.setup{}
-                lspconfig.clangd.setup{}
-                lspconfig.emmet_ls.setup{}
+        lspconfig.lua_ls.setup{
+            settings = {
+                Lua = {
+                    completion = {
+                        callSnippet = "Replace",
+                    },
+                }}
+            }
+            lspconfig.tsserver.setup{}
+            lspconfig.ruby_lsp.setup{}
+            lspconfig.html.setup{}
+            lspconfig.cssls.setup{}
+            lspconfig.jsonls.setup{}
+            lspconfig.clangd.setup{}
+            lspconfig.emmet_ls.setup{}
         end
     }
