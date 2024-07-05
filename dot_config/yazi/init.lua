8322df4fde40d5f5793f87ca4dd8fd90377fa92c
Yushi <yushi_61@proton.me>
2024-07-05 23:09:44 +0100

    feat: update yatline plugin

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -26,3 +26,83 @@ function Manager:render(area)
 		Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
 	}
 end
+
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+
+	style_a = {
+		fg = "black",
+		bg_mode = {
+			normal = "#a89984",
+			select = "#d79921",
+			un_set = "#d65d0e"
+		}
+	},
+	style_b = { bg = "#665c54", fg = "#ebdbb2" },
+	style_c = { bg = "#3c3836", fg = "#a89984" },
+
+	permissions_t_fg = "green",
+	permissions_r_fg = "yellow",
+	permissions_w_fg = "red",
+	permissions_x_fg = "cyan",
+	permissions_s_fg = "darkgray",
+
+	tab_width = 20,
+
+	selected = { icon = "󰻭", fg = "yellow" },
+	copied = { icon = "", fg = "green" },
+	cut = { icon = "", fg = "red" },
+
+	total = { icon = "󰮍", fg = "yellow" },
+	succ = { icon = "", fg = "green" },
+	fail = { icon = "", fg = "red" },
+	found = { icon = "󰮕", fg = "blue" },
+	processed = { icon = "󰐍", fg = "green" },
+
+	header_line = {
+		left = {
+			section_a = {
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		}
+	},
+	status_line = {
+		left = {
+			section_a = {
+        			{type = "string", custom = false, name = "tab_mode"},
+			},
+			section_b = {
+        			{type = "string", custom = false, name = "hovered_size"},
+			},
+			section_c = {
+        			{type = "string", custom = false, name = "hovered_name"},
+			}
+		},
+		right = {
+			section_a = {
+        			{type = "string", custom = false, name = "cursor_position"},
+			},
+			section_b = {
+        			{type = "string", custom = false, name = "cursor_percentage"},
+			},
+			section_c = {
+        			{type = "coloreds", custom = false, name = "permissions"},
+			}
+		}
+	},
+})
+
+
+
