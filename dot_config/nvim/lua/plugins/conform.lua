ff63b9715c9dacf9efb789539b19421b3615d115
Yushi <yushi_61@proton.me>
2024-07-09 09:52:01 +0100

    feat: update conform settings

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,22 @@
 require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "eslint" },
-				html = { "prettier" },
-				css = { "prettier" },
-				php = { "php_cs_fixer" },
-			},
-		})
-		-- format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
-			end,
-		})
+    formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "eslint" },
+        html = { "prettier" },
+        css = { "prettier" },
+        php = { "php_cs_fixer" },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 10000,
+        lsp_format = "fallback",
+    },
+})
+-- format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
+}
