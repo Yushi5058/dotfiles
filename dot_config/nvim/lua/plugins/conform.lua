c772fb2ff6cd5e33f7c3e9f0bbe0962ecaedf3a4
Yushi <yushi_61@proton.me>
2024-07-11 18:02:01 +0100

    feat: update formatter for faster formatting

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,21 @@
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "eslint" },
-        html = { "prettier" },
-        css = { "prettier" },
-        php = { "php_cs_fixer" },
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 10000,
-        lsp_format = "fallback",
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "eslintd" },
+		html = { "prettierd" },
+		css = { "prettierd" },
+		php = { "php_cs_fixer" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
 })
 -- format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-    end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
