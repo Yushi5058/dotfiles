d919c75278eb92ad0a2523e867c34f4a979303bf
Yushi <yushi_61@proton.me>
2024-07-19 17:22:54 +0100

    fix: correct mason installer path

--- dot_config/nvim/lua/config/deps.lua
+++ dot_config/nvim/lua/config/deps.lua
@@ -28,7 +28,8 @@ local add = MiniDeps.add
 add({
 	source = "neovim/nvim-lspconfig",
 	-- Supply dependencies near target plugin
-	depends = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "mason-tool-installer.nvim" },
+	depends = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
+
 })
 add({
 	source = "nvim-treesitter/nvim-treesitter",
