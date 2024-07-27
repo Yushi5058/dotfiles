9c7eab6b44e3bf7a3ad059191cefe08f5f260dc7
Yushi <yushi_61@proton.me>
2024-07-27 19:02:29 +0100

    feat(nvim): update treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,10 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "vimdoc", "javascript", "typescript", "c", "vim", "html", "css", "json", "php" },
-	highlight = { enable = true },
-	additional_vim_regex_highlighting = false,
-})
+return {
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "vimdoc", "javascript", "typescript", "c", "vim", "html", "css", "json", "php" },
+			highlight = { enable = true },
+			additional_vim_regex_highlighting = false,
+		})
+	end,
+}
