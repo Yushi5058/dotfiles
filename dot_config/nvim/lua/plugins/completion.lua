c39888f8f0c5682970cea99a67b70dc2da928e3f
Yushi <yushi_61@proton.me>
2024-07-27 19:00:12 +0100

    feat(nvim): update completion and grapple configs

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -1,44 +1,57 @@
-local cmp = require("cmp")
-require("luasnip.loaders.from_vscode").lazy_load()
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-c>"] = cmp.mapping.abort(),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip", max_item_count = 3 }, -- snippets
-		{ name = "buffer", max_item_count = 5 }, -- text within current buffer
-		{ name = "path", max_item_count = 3 },
-	}),
-})
+	config = function()
+		local cmp = require("cmp")
+		require("luasnip.loaders.from_vscode").lazy_load()
 
--- `:` cmdline setup.
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{
-			name = "cmdline",
-			option = {
-				ignore_cmds = { "Man", "!" },
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
 			},
-		},
-	}),
-})
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-c>"] = cmp.mapping.abort(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip", max_item_count = 3 }, -- snippets
+				{ name = "buffer", max_item_count = 5 }, -- text within current buffer
+				{ name = "path", max_item_count = 3 },
+			}),
+		})
+
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+	end,
+}
--- dot_config/nvim/lua/plugins/grapple.lua
+++ dot_config/nvim/lua/plugins/grapple.lua
@@ -1,5 +1,10 @@
-local map = vim.keymap.set
-        map("n","<leader>a", "<cmd>Grapple toggle<cr>")
-        map("n","<C-e>", "<cmd>Grapple toggle_tags<cr>")
-        map("n","<C-n>", "<cmd>Grapple cycle_tags next<cr>")
-        map("n","<C-p>", "<cmd>Grapple cycle_tags prev<cr>")
+return {
+	"cbochs/grapple.nvim",
+	config = function()
+		local map = vim.keymap.set
+		map("n", "<leader>a", "<cmd>Grapple toggle<cr>")
+		map("n", "<C-e>", "<cmd>Grapple toggle_tags<cr>")
+		map("n", "<C-n>", "<cmd>Grapple cycle_tags next<cr>")
+		map("n", "<C-p>", "<cmd>Grapple cycle_tags prev<cr>")
+	end,
+}
