b0e4eb410459dcd3b5078b9f7d1ae632eb158cd5
Yushi <yushi_61@proton.me>
2025-11-17 11:08:31 +0100

    fix: update lsp config

---
+++ .config/nvim/lua/config/lsp.lua
@@ -0,0 +1,92 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
+
+		-- Rename the variable under your cursor.
+		--  Most Language Servers support renaming across files, etc.
+		map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+
+		-- Find references for the word under your cursor.
+		map("grr", MiniExtra.pickers.lsp({ scope = "references" }), "[G]oto [R]eferences")
+
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map("gri", MiniExtra.pickers.lsp({ scope = "implementation" }), "[G]oto [I]mplementation")
+
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-t>.
+		map("grd", MiniExtra.pickers.lsp({ scope = "definition" }), "[G]oto [D]efinition")
+
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header.
+		map("grD", MiniExtra.pickers.lsp({ scope = "declaration" }), "[G]oto [D]eclaration")
+
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map("gO", MiniExtra.pickers.lsp({ scope = "document_symbol" }), "Open Document Symbols")
+
+		-- Fuzzy find all the symbols in your current workspace.
+		--  Similar to document symbols, except searches over your entire project.
+		map("gW", MiniExtra.pickers.lsp({ scope = "workspace_symbol" }), "Open Workspace Symbols")
+
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is and you want to see
+		--  the definition of its *type*, not where it was *defined*.
+		map("grt", MiniExtra.pickers.lsp({ scope = "type_definition" }), "[G]oto [T]ype Definition")
+
+		-- The following two autocommands are used to highlight references of the
+		-- word under your cursor when your cursor rests there for a little while.
+		--
+		-- When you move your cursor, the highlights will be cleared (the second autocommand).
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if
+			client
+			and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+		then
+			local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
+
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.clear_references,
+			})
+
+			vim.api.nvim_create_autocmd("LspDetach", {
+				group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+				end,
+			})
+		end
+	end,
+})
+
+-- vim.lsp.inlay_hint.enable(true)
+
+local severity = vim.diagnostic.severity
+
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[severity.ERROR] = " ",
+			[severity.WARN] = " ",
+			[severity.HINT] = "󰠠 ",
+			[severity.INFO] = " ",
+		},
+	},
+})
