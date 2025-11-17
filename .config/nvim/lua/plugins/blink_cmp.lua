cf65b9880437467b71d6a75c498643888f8b0bba
Yushi <yushi_61@proton.me>
2025-11-17 11:16:09 +0100

    fix: update lsp config

--- .config/nvim/lua/plugins/blink_cmp.lua
+++ .config/nvim/lua/plugins/blink_cmp.lua
@@ -1,30 +1,33 @@
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	event = "VimEnter",
-	dependencies = {
-		{ "rafamadriz/friendly-snippets" },
-		{ "folke/lazydev.nvim", opts = {} },
-	},
+	dependencies = { "rafamadriz/friendly-snippets" },
+
 	version = "1.*",
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
 		keymap = { preset = "default" },
 
 		appearance = {
-
 			nerd_font_variant = "mono",
 		},
 
+		-- (Default) Only show the documentation popup when manually triggered
 		completion = { documentation = { auto_show = false } },
 
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "lazydev" },
-			providers = {
-				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
-		fuzzy = { implementation = "lua" },
-		signature = { enabled = true },
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
+	opts_extend = { "sources.default" },
 }
--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,54 +1,54 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-          "lua_ls",
-          "bashls",
-          "clangd",
-          "cssls",
-          "djls",
-          "emmet_ls",
-          "html",
-          "intelephense",
-          "marksman",
-          "postgres_lsp",
-          "pyright",
-          "tailwindcss",
-          "ts_ls",
-          "vue_ls"
-        },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        },
-      },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "prettierd", -- prettier formatter
-        "stylua", -- lua formatter
-        "ruff", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    },
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"bashls",
+				"clangd",
+				"cssls",
+				"djls",
+				"emmet_ls",
+				"html",
+				"intelephense",
+				"marksman",
+				"postgres_lsp",
+				"pyright",
+				"tailwindcss",
+				"ts_ls",
+				"vue_ls",
+			},
+		},
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"prettierd", -- prettier formatter
+				"stylua", -- lua formatter
+				"ruff", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
 }
