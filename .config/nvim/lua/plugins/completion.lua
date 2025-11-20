f4e44faaf5d0da249671cfe6a4049fda9e9d311f
Yushi <yushi_61@proton.me>
2025-11-20 22:54:35 +0100

    feat: update blink keymaps

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -7,10 +7,13 @@ return {
 	opts = {
 		keymap = {
 			preset = "default",
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-d>"] = { "show", "show_documentation", "hide_documentation" },
 		},
 
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-}
+	},
 }
