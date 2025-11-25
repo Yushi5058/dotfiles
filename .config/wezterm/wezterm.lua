bc17226164e2f246ac353156b5046254997a5a28
Yushi <yushi_61@proton.me>
2025-11-25 15:07:30 +0100

    feat(wezterm): decrease font size and disable tab_bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font_size = 20
+config.font_size = 16
 config.font = wezterm.font("Maple Mono", { weight = "Light", stretch = "Normal", style = "Normal" })
 config.color_scheme = "rose-pine-moon"
+config.enable_tab_bar = false
 
 return config
