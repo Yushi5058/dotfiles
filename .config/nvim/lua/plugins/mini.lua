36c1b68835b359c5923d66a23ca75c64692b9616
Yushi <yushi_61@proton.me>
2025-12-11 13:27:54 +0100

    feat(nvim): move to mini.completion and add mini.snippets'

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,60 @@
 return {
 	"echasnovski/mini.nvim",
+	dependencies = { "rafamadriz/friendly-snippets" },
 	config = function()
-		-- require("mini.completion").setup()
+		require("mini.completion").setup({
+			lsp_completion = {
+				source_func = "completefunc",
+				auto_setup = false,
+			},
+			mappings = {
+				scroll_down = "<C-d",
+				scroll_up = "<C-u",
+			},
+		})
+		-- 3. CUSTOM KEY MAPPINGS
+		local map = vim.keymap.set
+		local opts = { expr = true, replace_keycodes = true }
+
+		-- CTRL+j: Move Down (Next Item)
+		map("i", "<C-j>", function()
+			return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-j>"
+		end, opts)
+
+		-- CTRL+k: Move Up (Previous Item)
+		map("i", "<C-k>", function()
+			return vim.fn.pumvisible() == 1 and "<C-p>" or "<C-k>"
+		end, opts)
+
+		-- CTRL+y: Accept (Yes)
+		-- This is native, but we ensure it works if the menu is open.
+		map("i", "<C-y>", function()
+			return vim.fn.pumvisible() == 1 and "<C-y>" or "<C-y>"
+		end, opts)
+
+		-- CR (Enter): STOP it from accepting completion
+		-- If menu is open: Close it (<C-e>) and insert Newline (<CR>)
+		-- If menu is closed: Just insert Newline
+		map("i", "<CR>", function()
+			if vim.fn.pumvisible() == 1 then
+				return vim.api.nvim_replace_termcodes("<C-e><CR>", true, true, true)
+			else
+				return vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+			end
+		end, { expr = true })
+		-- snippets
+		local gen_loader = require("mini.snippets").gen_loader
+		require("mini.snippets").setup({
+			snippets = {
+				-- Load custom file with global snippets first
+				-- gen_loader.from_file("~/.config/nvim/snippets/global.json"),
+
+				-- Load snippets based on current language by reading files from
+				-- "snippets/" subdirectories from 'runtimepath' directories.
+				gen_loader.from_lang(),
+			},
+		})
+
 		-- waiting for mini.snippets
 		require("mini.pick").setup({
 			mappings = {
