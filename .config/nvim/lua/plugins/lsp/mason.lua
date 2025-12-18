375183289a336658ae48f535cbe5b043d71968e1
Yushi <yushi_61@proton.me>
2025-12-19 00:47:58 +0100

    feat(lsp): update vim.diagnostics configuration

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -83,29 +83,15 @@ return {
 			run_on_start = true,
 		},
 	},
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy",
-	priority = 1000,
-	config = function()
-		require("tiny-inline-diagnostic").setup({
-			preset = "modern",
-			hi = {
-				error = "DiagnosticError",
-				warn = "DiagnosticWarn",
-				info = "DiagnosticInfo",
-				hint = "DiagnosticHint",
-			},
-			options = {
-				add_messages = {
-					display_count = true,
-					messages = true,
-				},
-				multilines = {
-					always_show = true,
-					enabled = true,
-				},
-			},
-		})
-		vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
-	end,
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+				preset = "ghost",
+			})
+			vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+		end,
+	},
 }
