3589e43c9ed2c07b58e0a599bbe9fdf8f559d892
Yushi <yushi_61@proton.me>
2025-12-18 23:54:36 +0100

    fix(plugin): migrate to new colorizer plugin

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,17 +1,4 @@
 return {
-	"NvChad/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup({
-			filetypes = { "css", "html", "scss" },
-			user_default_options = {
-				RRGGBBAA = true,
-				rgb_fn = true,
-				hsl_fn = true,
-				tailwind = true,
-				sass = { enable = true, parsers = { "css" } },
-				always_update = true,
-				mode = virtualtext,
-			},
-		})
-	end,
+	"brenoprata10/nvim-highlight-colors",
+	opts = {},
 }
