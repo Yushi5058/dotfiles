2e06b44361d390f9cd1f96e2d0bbedafa05df9ef
Yushi <yushi_61@proton.me>
2026-02-08 12:55:01 +0100

    feat(wezterm/nvim): configure arabic rendering

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,9 +2,15 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 16
-config.font = wezterm.font("Maple Mono", { weight = "Light", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font_with_fallback({
+	"Maple Mono",
+	"FreeSans",
+	"Arial",
+})
 config.color_scheme = "rose-pine-moon"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.6
+config.unicode_version = 14
+config.bidi_enabled = true
 
 return config
