d71d92a9ec8c620cdb42bcd7cb2781b2da000fab
Yushi <yushi_61@proton.me>
2026-04-04 10:41:24 +0100

    feat(nvim): add which-key and keymaps snacks.picker plugin for keybindings cheatsheet

---
+++ nvim/.config/nvim/lua/plugins/keybindings.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			picker = { enabled = true },
+		},
+		keys = {
+			{
+				"<leader>sk",
+				function()
+					Snacks.picker.keymaps()
+				end,
+				desc = "Keymaps",
+			},
+			{
+				"<leader>sc",
+				function()
+					Snacks.picker.commands()
+				end,
+				desc = "Commands",
+			},
+		},
+	},
+}
