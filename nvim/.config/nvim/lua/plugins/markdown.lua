d1fd60db933a5f432caeba05d50009b3c20ecbcb
Yushi <yushi_61@proton.me>
2026-04-20 15:45:28 +0100

    treesitter: remove dependency from markdown plugin

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+	dependencies = { "nvim-mini/mini.nvim" },
 	opts = { completions = { lsp = { enabled = true } } },
 	keys = {
 		{ "<leader>g", "<cmd>RenderMarkdown preview<cr>", desc = "Markdown Preview" },
