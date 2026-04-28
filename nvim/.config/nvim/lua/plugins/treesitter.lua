e43de2339c9378db8e0ac7a612ee1e65e2b07f4a
Yushi <yushi_61@proton.me>
2026-04-28 21:00:31 +0100

    fix(nvim): update arborist options

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
 	"https://github.com/arborist-ts/arborist.nvim",
 	config = function()
-		require("arborist").setup()
+		require("arborist").setup({
+			prefer_wasm = false,
+			update_cadence = "weekly",
+			compiler = "zig",
+		})
 	end,
 }
