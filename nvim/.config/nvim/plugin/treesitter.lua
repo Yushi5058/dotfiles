608e5932ea75de0a5aa72bffde7d65183ca9ce11
Yushi <yushi_61@proton.me>
2026-05-10 14:32:59 +0100

    fix(nvim): treesitter setup with PackChanged autocmd

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -2,13 +2,29 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
-require("lazyload").on_vim_enter(function()
+local function setup_treesitter()
   vim.cmd("packadd nvim-treesitter")
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = "all",
-    sync_install = false,
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
+  local ok, configs = pcall(require, "nvim-treesitter.configs")
+  if ok then
+    configs.setup({
+      ensure_installed = "all",
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+end
+
+local has_treesitter, _ = pcall(require, "nvim-treesitter.configs")
+if has_treesitter then
+  setup_treesitter()
+else
+  vim.api.nvim_create_autocmd("PackChanged", {
+    callback = function(ev)
+      if ev.data and ev.data.spec and ev.data.spec.name == "nvim-treesitter" then
+        vim.defer_fn(setup_treesitter, 100)
+      end
+    end,
   })
-end)
+end
