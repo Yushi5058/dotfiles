590fdc91da2f57b5fb01fa29cd6e207676001238
Yushi <yushi_61@proton.me>
2026-05-10 14:31:18 +0100

    fix(nvim): proper blink.cmp config with blink.lib dependency
    - Add blink.lib as dependency
    - Add friendly-snippets
    - Use proper build():wait() and cmp.setup() from official docs
    - Fix database.lua to use correct path

--- nvim/.config/nvim/plugin/blink.lua
+++ nvim/.config/nvim/plugin/blink.lua
@@ -1,54 +1,25 @@
 vim.pack.add({
-  { src = "https://github.com/hrsh7th/nvim-cmp" },
+  { src = "https://github.com/saghen/blink.lib" },
 })
 vim.pack.add({
-  { src = "https://github.com/hrsh7th/cmp-vsnip" },
-})
-vim.pack.add({
-  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-})
-vim.pack.add({
-  { src = "https://github.com/hrsh7th/cmp-nvim-lua" },
-})
-vim.pack.add({
-  { src = "https://github.com/hrsh7th/cmp-buffer" },
-})
-vim.pack.add({
-  { src = "https://github.com/hrsh7th/cmp-path" },
+  { src = "https://github.com/Saghen/blink.cmp" },
 })
 vim.pack.add({
   { src = "https://github.com/rafamadriz/friendly-snippets" },
 })
 
 require("lazyload").on_vim_enter(function()
-  vim.cmd("packadd nvim-cmp")
+  vim.cmd("packadd blink.cmp")
 
-local cmp = require("cmp")
-cmp.setup({
-  snippet = { expand = function(args)
-    vim.snippet.expand(args.body)
-  end,
-  mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "vsnip" },
-    { name = "nvim_lua" },
-    { name = "buffer" },
-    { name = "path" },
-  }),
-})
-
-vim.keymap.set("n", "<leader>e", function() require("mini.files").open(vim.api.nvim_buf_get_name(0)) end, { desc = "Explorer" })
+  local cmp = require("blink.cmp")
+  cmp.build():wait(60000)
+  cmp.setup({
+    keymap = { preset = "default" },
+    completion = {
+      documentation = { auto_show = false },
+      trigger = { show_on_insert_on_trigger_character = true },
+    },
+    sources = { default = { "lsp", "path", "snippets", "buffer" } },
+    fuzzy = { implementation = "rust" },
+  })
+end)
--- nvim/.config/nvim/plugin/database.lua
+++ nvim/.config/nvim/plugin/database.lua
@@ -1,19 +1,19 @@
--- Database plugin (disabled due to vim.pack compatibility issue)
--- vim.pack.add({
---   { src = "https://github.com/kndndrj/nvim-dbee" },
--- })
--- vim.pack.add({
---   { src = "https://github.com/MunifTanjim/nui.nvim" },
--- })
---
--- require("lazyload").on_vim_enter(function()
---   vim.cmd("packadd nvim-dbee")
---   vim.cmd("packadd nui.nvim")
---   require("dbee").setup()
---
---   vim.keymap.set("n", "<leader>bt", function() require("dbee").toggle() end, { desc = "DBee: Toggle UI" })
---   vim.keymap.set("n", "<leader>be", function() require("dbee").execute() end, { desc = "DBee: Execute Query" })
---   vim.keymap.set("n", "<leader>bs", function() require("dbee").sidebar_toggle() end, { desc = "DBee: Toggle Sidebar" })
---   vim.keymap.set("n", "<leader>ba", function() require("dbee").execute() end, { desc = "DBee: Action (Execute)" })
---   vim.keymap.set("n", "<leader>bq", function() require("dbee").close() end, { desc = "DBee: Quit/Close" })
--- end)
+vim.pack.add({
+  { src = "https://github.com/MunifTanjim/nui.nvim", start = true },
+})
+vim.pack.add({
+  { src = "https://github.com/kndndrj/nvim-dbee" },
+})
+
+require("lazyload").on_vim_enter(function()
+  local ok, _ = pcall(require, "dbee")
+  if ok then
+    require("dbee").setup()
+
+    vim.keymap.set("n", "<leader>bt", function() require("dbee").toggle() end, { desc = "DBee: Toggle UI" })
+    vim.keymap.set("n", "<leader>be", function() require("dbee").execute() end, { desc = "DBee: Execute Query" })
+    vim.keymap.set("n", "<leader>bs", function() require("dbee").sidebar_toggle() end, { desc = "DBee: Toggle Sidebar" })
+    vim.keymap.set("n", "<leader>ba", function() require("dbee").execute() end, { desc = "DBee: Action (Execute)" })
+    vim.keymap.set("n", "<leader>bq", function() require("dbee").close() end, { desc = "DBee: Quit/Close" })
+  end
+end)
