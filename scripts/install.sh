c74b5390426c9ecd4bbeb2b1e4a5b2b1273edc78
Yushi <yushi_61@proton.me>
2026-06-06 19:38:55 +0100

    feat: overhaul install script - add new packages, remove bun, restructure sections

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,15 +8,15 @@ echo "Installing packages..."
 # Core development & Daily use
 sudo pacman -S --noconfirm \
     git base-devel curl wget \
-    neovim python python-pip nodejs npm bun \
+    neovim python python-pip nodejs npm \
     go rust zig \
     bat eza fd ripgrep fzf \
     zsh starship \
     ghostty \
-    waybar mako sway swaylock \
+    waybar mako sway \
     grim slurp wl-clipboard \
     brightnessctl playerctl \
-    wireplumber pipewire \
+    wireplumber pipewire pipewire-alsa pipewire-jack pipewire-pulse \
     wlsunset \
     autotiling \
     cliphist \
@@ -29,8 +29,14 @@ sudo pacman -S --noconfirm \
     bluetui impala \
     dbeaver tree-sitter-cli \
     atuin croc git-delta github-cli \
-    jless jq localsend ouch parallel \
-    podman podman-compose ruff tldr tmate waydroid
+    jless jq lazygit localsend ouch parallel \
+    podman podman-compose ruff tldr tmate waydroid \
+    earlyoom zram-generator ufw pacman-contrib \
+    polkit-gnome ly \
+    wl-clip-persist wlr-randr \
+    xdg-desktop-portal-gtk \
+    qt5-wayland qt6-wayland qt5ct qt6ct kvantum kvantum-qt5 \
+    udiskie
 
 # ThinkPad X1 / Laptop support
 sudo pacman -S --noconfirm \
@@ -40,21 +46,35 @@ sudo pacman -S --noconfirm \
     fprintd iio-sensor-proxy \
     intel-media-driver libva-intel-driver \
     vulkan-intel \
-    bluez bluez-utils \
+    bluez bluez-utils blueman \
     pavucontrol \
     man-db man-pages \
-    noto-fonts noto-fonts-emoji \
+    noto-fonts noto-fonts-emoji noto-fonts-cjk \
     gnome-keyring libsecret \
     xdg-desktop-portal xdg-desktop-portal-wlr \
     unzip unrar p7zip \
     exfatprogs ntfs-3g \
     reflector
 
+# Fonts
+sudo pacman -S --noconfirm \
+    ttf-jetbrains-mono-nerd \
+    ttf-ubuntu-font-family \
+    ttf-font-awesome \
+    woff2-font-awesome
+
+# Virtualization
+sudo pacman -S --noconfirm \
+    virt-manager libvirt
+
 # AUR packages (requires paru)
 paru -S --noconfirm \
     librewolf-bin \
     zsh-antidote \
-    vscodium-bin
+    vscodium-bin \
+    swaylock-effects-git \
+    rose-pine-cursor \
+    maplemono-ttf
 
 # Dotfiles
 cd ~
